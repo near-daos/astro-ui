236c07f5614fe09be0b54aac2df213d5ba8e236d
MelonCode <qlniklp@gmail.com>
2021-07-13 14:00:22 +0300

    build(storybook): webpack 5 + sass styles support
    Switched to latest version of storybook for webpack5 support
    Added plugin and config for proper styles resolution

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,23 @@
+const path = require('path');
 module.exports = {
+  core: {
+    builder: 'webpack5'
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader?modules&importLoaders', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    });
+
+    // Return the altered config
+    return config;
+  },
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)'
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '!style-loader!css-loader!sass-loader!../styles/globals.scss';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
