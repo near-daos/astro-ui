a1c61b425feb30c4d5a944b829a6a4d54d7a17f6
MelonCode <qlniklp@gmail.com>
2021-07-15 12:39:59 +0300

    build(eslint & storybook): fix typescript types import, fix absolute paths for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,16 @@ module.exports = {
   core: {
     builder: 'webpack5'
   },
+
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
+
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve('./')
+    ];
 
     // Make whatever fine-grained changes you need
     config.module.rules.push({
