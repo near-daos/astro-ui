78be27f95977f1226883d648a3e10e2fd47597fe
MelonCode <qlniklp@gmail.com>
2021-07-16 19:55:38 +0300

    fix(ui/checkbox): fixed bug with props / internal state comparison

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -14,7 +14,7 @@ export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
     const idRef = useRef(uniqueId('checkbox-'));
     const [checked, setChecked] = useState(props.defaultChecked);
 
-    const value = props.checked || checked;
+    const value = props.checked != null ? props.checked : checked;
     const IconComponent = value ? CheckedIcon : UncheckedIcon;
 
     return (
