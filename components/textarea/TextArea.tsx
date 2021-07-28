8cf2383b178722147e747113e8afaa40d96e3b71
MelonCode <qlniklp@gmail.com>
2021-07-28 18:48:32 +0300

    feat(ui): implement textarea component

---
+++ components/textarea/TextArea.tsx
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from 'react';
+import uniqueId from 'lodash.uniqueid';
+import classNames from 'classnames';
+import TextareaAutosize from 'react-textarea-autosize';
+import { Property } from 'csstype';
+import inputStyles from 'components/input/input.module.scss';
+import styles from './textarea.module.scss';
+
+export interface TextAreaProps
+  extends React.ComponentProps<typeof TextareaAutosize> {
+  label?: string | undefined;
+  size: 'medium' | 'large' | 'block';
+  textAlign: Property.TextAlign;
+  resize: Property.Resize;
+}
+
+const sizeClasses = {
+  small: inputStyles['size-small'],
+  medium: inputStyles['size-medium'],
+  large: inputStyles['size-large'],
+  block: inputStyles['size-block']
+};
+
+export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (
+    {
+      label,
+      className: classNameProp,
+      maxLength,
+      size,
+      resize = 'none',
+      minRows = 8,
+      maxRows = 8,
+      textAlign,
+      ...props
+    },
+    externalRef
+  ) => {
+    const idRef = useRef(uniqueId('input-'));
+
+    const className = classNames(styles.textarea, sizeClasses[size]);
+
+    const [currentLength, setCurrentLength] = useState(0);
+
+    return (
+      <label className={className} htmlFor={props.id || idRef.current}>
+        {label && label.length > 0 && (
+          <span className={inputStyles.label}>{label}</span>
+        )}
+        <TextareaAutosize
+          {...props}
+          onChange={e => {
+            props.onChange?.(e);
+            setCurrentLength(e.currentTarget.value?.length);
+          }}
+          minRows={minRows}
+          maxRows={maxRows}
+          className={classNameProp}
+          id={idRef.current}
+          ref={externalRef}
+          style={{
+            textAlign,
+            resize
+          }}
+        />
+        {maxLength != null && (
+          <span
+            className={classNames(styles.length, {
+              [styles.error]: currentLength > maxLength,
+              [styles.success]: currentLength > 0 && currentLength < maxLength
+            })}
+          >
+            {currentLength}/{maxLength}
+          </span>
+        )}
+      </label>
+    );
+  }
+);
