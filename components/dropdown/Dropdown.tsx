773d6e25b6c0dae4407b1ebfe3bc7477fd1a43e5
MelonCode <qlniklp@gmail.com>
2021-07-29 19:07:49 +0300

    feat(ui): implement dropdown component and story

---
+++ components/dropdown/Dropdown.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect } from 'react';
+import classNames from 'classnames';
+import { useSelect } from 'downshift';
+import { IconButton } from 'components/button/IconButton';
+import { Icon } from 'components/Icon';
+import { Title } from 'components/Typography';
+import styles from './dropdown.module.scss';
+
+interface Option {
+  value: string;
+  label: string;
+  disabled?: boolean;
+}
+
+export interface DropdownProps {
+  value?: string;
+  className?: string;
+  onChange?: (value?: string) => void;
+  options: Option[];
+  defaultValue?: string | undefined;
+  placeholder?: string;
+}
+
+export const Dropdown: React.VFC<DropdownProps> = ({
+  options,
+  className: classNameProp,
+  onChange,
+  value,
+  defaultValue,
+  placeholder
+}) => {
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getMenuProps,
+    selectedItem,
+    selectItem,
+    highlightedIndex,
+    getItemProps
+  } = useSelect({
+    items: options,
+    itemToString: item => (item != null ? item.label : ''),
+    selectedItem: value
+      ? options.find(option => option.value === value)
+      : undefined,
+    onSelectedItemChange(changes) {
+      onChange?.(changes.selectedItem?.value);
+    },
+    defaultSelectedItem: defaultValue
+      ? options.find(option => option.value === defaultValue)
+      : undefined
+  });
+
+  const className = classNames(styles.dropdown, classNameProp);
+
+  /* Making sure that default value would cause onChange call */
+  useEffect(() => {
+    if (!value && defaultValue && !selectedItem) {
+      const defaultOption = options.find(
+        option => option.value === defaultValue
+      );
+
+      if (defaultOption != null) {
+        selectItem(defaultOption);
+      }
+    }
+  }, [selectedItem, options, value, selectItem, defaultValue]);
+
+  return (
+    <div className={className}>
+      <Title size={3} {...getToggleButtonProps()}>
+        {selectedItem?.label || placeholder}
+        <IconButton className={styles.icon}>
+          <Icon width={24} name="buttonArrowDown" />
+        </IconButton>
+      </Title>
+      <ul className={styles.menu} {...getMenuProps()}>
+        {isOpen &&
+          options.map((item, index) => (
+            <li
+              key={item.value}
+              className={classNames(
+                styles.item,
+                highlightedIndex === index ? styles.active : undefined
+              )}
+              {...getItemProps({
+                disabled: item.disabled,
+                item,
+                index
+              })}
+            >
+              {item.label}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
---
+++ stories/Dropdown.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { Dropdown } from 'components/dropdown/Dropdown';
+
+export default {
+  title: 'Components/Dropdown',
+  component: Dropdown
+} as Meta;
+
+const options = [
+  {
+    label: 'Test 1',
+    value: 'test-1'
+  },
+  {
+    label: 'Test 2',
+    value: 'test-2'
+  },
+  {
+    label: 'Test 3',
+    value: 'test-3'
+  }
+];
+
+export const Template = (
+  args: React.ComponentProps<typeof Dropdown>
+): JSX.Element => {
+  return (
+    <div>
+      <style jsx>
+        {`
+          div {
+            display: flex;
+            flex-direction: column;
+          }
+        `}
+      </style>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Dropdown {...args} />
+    </div>
+  );
+};
+
+Template.storyName = 'Dropdown';
+
+Template.args = {
+  options,
+  defaultValue: 'test-1'
+};
