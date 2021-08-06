875a276d853b76fd59dcc03a21553064204607cc
MelonCode <qlniklp@gmail.com>
2021-08-06 13:27:10 +0300

    feat(ui): implement sidebar dao item component, list component, and stories

---
+++ components/nav-dao/DaoHeader.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import cn from 'classnames';
+import { Icon } from 'components/Icon';
+import Image from 'next/image';
+import styles from './nav-dao.module.scss';
+
+interface DAOHeaderProps extends React.HTMLProps<HTMLHeadingElement> {
+  label: string;
+  className?: string;
+  isOpen?: boolean;
+  logo?: string;
+}
+
+export const DaoHeader: React.VFC<DAOHeaderProps> = ({
+  label,
+  className,
+  isOpen,
+  logo,
+  ...props
+}) => {
+  return (
+    <div {...props} className={cn(styles.header, className)}>
+      {logo ? (
+        <Image
+          src={logo}
+          width={24}
+          height={24}
+          alt={`${label} Dao Logo`}
+          placeholder="blur"
+          blurDataURL="/flag.svg"
+        />
+      ) : (
+        <Icon width={24} height={24} name="flag" />
+      )}
+      <h3> {label} </h3>
+      <Icon
+        style={{
+          marginLeft: 'auto',
+          transform: isOpen ? undefined : 'rotate(-90deg)',
+          transition: 'all 100ms'
+        }}
+        name="buttonArrowDown"
+      />
+    </div>
+  );
+};
