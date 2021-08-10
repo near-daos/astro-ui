b2df1cfada4e1adef7d1807a976e67b368757729
MelonCode <qlniklp@gmail.com>
2021-08-10 18:06:29 +0300

    build(styles): implement camelCase css modules support, get rid of next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,10 +1,32 @@
 const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const svgSprite = require('./plugins/next-svg-sprites');
+const cssLoaderConfig = require('./plugins/css-loader-config');
 
-module.exports = withPlugins([svgSprite], {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
-});
+module.exports = (phase, { defaultConfig }) => {
+  const plugins = [svgSprite, cssLoaderConfig];
+
+  const transformers = [];
+  const nextConfig = plugins.reduce(
+    (acc, next) => {
+      const nextConfig = next(acc);
+      if (typeof nextConfig.webpack === 'function') {
+        transformers.push(nextConfig.webpack);
+      }
+
+      return nextConfig;
+    },
+    {
+      ...defaultConfig,
+      reactStrictMode: true,
+      sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')]
+      }
+    }
+  );
+
+  nextConfig.webpack = (config, options) =>
+    transformers.reduce((acc, next) => next(acc, options), config);
+
+  return nextConfig;
+};
