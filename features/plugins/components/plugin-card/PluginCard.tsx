5208e134645a96fd364699a64208b116e7fcd6fa
overzunov <olegverzunov@gmail.com>
2021-08-12 15:11:13 +0000

    Merged in feature/CU-p7b47q-plugin-card (pull request #58)
    feat(plugin card): implemented plugin card component, story
    
    * feat(plugin card): implemented plugin card component, story
    
    
    Approved-by: Dmitriy Karpov

---
+++ features/plugins/components/plugin-card/PluginCard.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import cn from 'classnames';
+
+import StatusPanel from 'components/cards/proposal-card/components/proposal-status-panel/ProposalStatusPanel';
+
+import styles from './plugin-card.module.scss';
+
+export interface PluginCardProps {
+  tokenName: string;
+  created: string;
+  functionName: string;
+}
+
+export const PluginCard: FC<PluginCardProps> = ({
+  tokenName,
+  created,
+  functionName
+}) => {
+  return (
+    <div className={cn(styles.root)}>
+      <StatusPanel type="Call NEAR function" status="Passed" />
+      <div className={styles.content}>
+        <div className={styles.token}>{tokenName}</div>
+        <div className={styles.created}>{created}</div>
+        <div className={styles.function}>{functionName}</div>
+      </div>
+    </div>
+  );
+};
