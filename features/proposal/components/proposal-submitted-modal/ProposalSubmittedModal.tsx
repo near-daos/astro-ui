2218c1f82b6c8b84bd12fd94227323d091370c2e
MelonCode <qlniklp@gmail.com>
2021-08-17 15:07:54 +0300

    feat(proposal): add proposal confimation modal and story

---
+++ features/proposal/components/proposal-submitted-modal/ProposalSubmittedModal.tsx
@@ -0,0 +1,29 @@
+import { Button } from 'components/button/Button';
+import { Modal } from 'components/modal';
+import styles from 'features/create-dao/components/settings-modal/dao-settings-modal.module.scss';
+import { ProposalSubmittedIllustration } from 'features/proposal/components/proposal-submitted-modal/illustration';
+import React, { VFC } from 'react';
+
+export interface ProposalSubmittedModalProps {
+  isOpen: boolean;
+  onClose: (...args: unknown[]) => void;
+}
+
+export const ProposalSubmittedModal: VFC<ProposalSubmittedModalProps> = ({
+  isOpen,
+  onClose
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className={styles.root}>
+        <ProposalSubmittedIllustration />
+        <section>
+          <h2> Your proposal was added! </h2>
+          <p>Others can vote on it now. </p>
+        </section>
+
+        <Button onClick={() => onClose()}> Cool! </Button>
+      </div>
+    </Modal>
+  );
+};
---
+++ stories/proposal/ProposalSubmittedModal.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, Story } from '@storybook/react';
+import { Button } from 'components/button/Button';
+import { ModalProvider, useModal } from 'components/modal';
+
+import {
+  ProposalSubmittedModal,
+  ProposalSubmittedModalProps
+} from 'features/proposal/components/proposal-submitted-modal/ProposalSubmittedModal';
+import React, { FC, useCallback } from 'react';
+
+export default {
+  title: 'Features/Proposal/Popups/Proposal Submitted Modal',
+  decorators: [story => <ModalProvider>{story()}</ModalProvider>]
+} as Meta;
+
+const DemoComponent: FC<{ modalProps: ProposalSubmittedModalProps }> = ({
+  modalProps
+}) => {
+  const [showModal] = useModal<ProposalSubmittedModalProps>(
+    ProposalSubmittedModal,
+    {
+      ...modalProps
+    }
+  );
+
+  const handleClick = useCallback(async () => {
+    await showModal();
+  }, [showModal]);
+
+  return (
+    <Button type="button" onClick={handleClick}>
+      Show Modal
+    </Button>
+  );
+};
+
+export const Template: Story<ProposalSubmittedModalProps> = (
+  args
+): JSX.Element => <DemoComponent modalProps={args} />;
+
+Template.storyName = 'Proposal Submitted Modal';
