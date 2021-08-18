de75b58ae275cde2dee7baf777baf0dd08c328b7
Overzunov <overzunov@lohika.com>
2021-08-18 16:23:24 +0300

    feat(stake token popup): implemented Stake token popup, story, Token amount input

---
+++ features/voting-token/components/stake-tokens-popup/StakeTokensPopup.tsx
@@ -0,0 +1,98 @@
+import React, { FC, FormEvent, useCallback, useState } from 'react';
+import cn from 'classnames';
+
+import { AmountInput } from 'features/voting-token/components/amount-input';
+import { Modal } from 'components/modal';
+import { Button } from 'components/button/Button';
+import { Icon, IconName } from 'components/Icon';
+
+import styles from './stake-tokens-popup.module.scss';
+
+type Token = {
+  id: string;
+  icon?: IconName;
+  name: string;
+  symbol: string;
+  balance: number;
+};
+
+export interface StakeTokensPopupProps {
+  isOpen: boolean;
+  onClose: (...args: unknown[]) => void;
+  token: Token;
+  rate: number;
+}
+
+export const StakeTokensPopup: FC<StakeTokensPopupProps> = ({
+  isOpen,
+  onClose,
+  token,
+  rate
+}) => {
+  const [value, setValue] = useState('');
+  const handleSubmit = useCallback(() => {
+    // todo - handle submit here
+    onClose('submitted');
+  }, [onClose]);
+
+  const handleMax = useCallback(() => {
+    setValue(`${token.balance}`);
+  }, [token.balance]);
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className={styles.root}>
+        <h2>Stake tokens</h2>
+        <div className={styles.content}>
+          <div className={styles.info}>
+            <div className={styles.token}>
+              <div className={styles.label}>Your voting token</div>
+              <div className={styles.value}>
+                <div className={styles.icon}>
+                  {token.icon && <Icon name={token.icon} />}
+                </div>
+                {token.name}
+              </div>
+            </div>
+            <div className={styles.symbol}>
+              <div className={styles.label}>&nbsp;</div>
+              <div className={styles.value}>{token.symbol}</div>
+            </div>
+            <div className={styles.balance}>
+              <div className={cn(styles.label, styles.right)}>Your balance</div>
+              <div className={cn(styles.value, styles.right)}>
+                <strong>{token.balance}</strong>
+                &nbsp;
+                {token.name}
+              </div>
+            </div>
+          </div>
+          <div className={styles.input}>
+            <AmountInput
+              label="Tokens amount"
+              placeholder="Input tokens amount"
+              tokenName="GOOSE"
+              onMax={handleMax}
+              value={value}
+              onChange={(e: FormEvent) => {
+                const val = (e.target as HTMLInputElement).value;
+
+                setValue(+val > token.balance ? `${token.balance}` : val);
+              }}
+              cost={+(value ?? 0) * rate}
+            />
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <Button variant="secondary" onClick={onClose} size="small">
+            Cancel
+          </Button>
+          &nbsp;&nbsp;
+          <Button variant="primary" onClick={handleSubmit} size="small">
+            Stake
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
