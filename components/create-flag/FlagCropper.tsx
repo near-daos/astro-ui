df4a0c865d18921070645093339ae1fe817877d4
MelonCode <qlniklp@gmail.com>
2021-09-01 15:26:52 +0300

    feat(flag-selector): implement undo/redo functionality and mobile mode

--- components/create-flag/FlagCropper.tsx
+++ components/create-flag/FlagCropper.tsx
@@ -1,25 +1,28 @@
 import cn from 'classnames';
 import styles from 'components/create-flag/create-flag.module.scss';
-import React, { useEffect, VFC } from 'react';
-import { Cropper } from 'react-cropper';
-import { useWindowSize } from 'react-use';
 import 'cropperjs/dist/cropper.css';
+import React, { useEffect, VFC } from 'react';
+import { Cropper, ReactCropperProps } from 'react-cropper';
+import { useWindowSize } from 'react-use';
 
-export type CreateFlagProps = {
+export interface CreateFlagProps
+  extends Omit<ReactCropperProps, 'dragMode' | 'onInitialized'> {
   cropperRef: React.MutableRefObject<Cropper | undefined>;
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
   dragMode?: boolean;
   src: string | undefined;
   className?: string | undefined;
   preview?: string | undefined;
-};
+}
+
 export const FlagCropper: VFC<CreateFlagProps> = ({
   dragMode,
   preview,
   cropperRef,
   canvasRef,
   src,
-  className
+  className,
+  ...cropperProps
 }) => {
   const { width, height } = useWindowSize();
 
@@ -35,7 +38,7 @@ export const FlagCropper: VFC<CreateFlagProps> = ({
   }, [width, height, cropperRef]);
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <Cropper
         responsive
         src={src}
@@ -59,8 +62,9 @@ export const FlagCropper: VFC<CreateFlagProps> = ({
         aspectRatio={1}
         center={false}
         guides={false}
+        {...cropperProps}
       />
       <canvas style={{ display: 'none' }} ref={canvasRef} />
-    </>
+    </div>
   );
 };
