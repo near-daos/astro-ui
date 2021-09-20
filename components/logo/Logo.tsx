76a8dd17d4b52933b7d146eba2adb13fdac85054
aponomarenko <alexey.a.ponomarenko@gmail.com>
2021-09-20 21:27:38 +0300

    feat(logo): updated app logo

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -1,8 +1,7 @@
-import { useRouter } from 'next/router';
 import React from 'react';
 import cn from 'classnames';
 import Link from 'next/link';
-import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import { Icon } from 'components/Icon';
 
@@ -10,20 +9,15 @@ import styles from './logo.module.scss';
 
 interface LogoProps {
   className?: string;
-  src?: string;
 }
 
-export const Logo: React.VFC<LogoProps> = ({ className, src, ...props }) => {
+export const Logo: React.VFC<LogoProps> = ({ className, ...props }) => {
   const router = useRouter();
 
   return (
     <Link href={{ href: '/dao/[dao]', query: router.query }} passHref>
       <a href="*" {...props} className={cn(styles.logo, className)}>
-        {src ? (
-          <Image loading="eager" src={src} width={48} height={48} />
-        ) : (
-          <Icon width={48} height={48} name="flag" />
-        )}
+        <Icon width={92} name="appLogo" />
       </a>
     </Link>
   );
