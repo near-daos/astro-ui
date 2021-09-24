fcc0e16fff459ffdb8fd2d15ecfe97a67102d1e1
unholywhite <s.krupnyk@gmail.com>
2021-09-24 13:55:05 +0300

    fix(create dao): add scrollbar to transparency page when content is too tall

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,11 +1,4 @@
-import React, {
-  FC,
-  memo,
-  ReactNode,
-  useCallback,
-  useState,
-  useEffect
-} from 'react';
+import React, { FC, memo, ReactNode, useCallback, useState } from 'react';
 import cn from 'classnames';
 import ReactModal from 'react-modal';
 
@@ -32,22 +25,6 @@ export const Modal: FC<ModalProps> = memo(
     hideCloseIcon
   }) => {
     const [open, setOpen] = useState(isOpen);
-
-    useEffect(() => {
-      const bodyEl = document.querySelector('body');
-
-      if (bodyEl) {
-        const overflow = open ? 'hidden' : 'auto';
-
-        bodyEl.style.overflow = overflow;
-      }
-
-      return () => {
-        if (bodyEl) {
-          bodyEl.style.overflow = 'auto';
-        }
-      };
-    }, [open]);
 
     const handleClose = useCallback(() => {
       onClose();
