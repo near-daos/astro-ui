75ffb852c9cbad5113bc6a07bb3e3f538e3e4145
Oleg Kalenik <okalenyk@gmail.com>
2021-09-29 12:17:20 +0300

    feat(deployment): Astro Web App K8s Deployment
    - added Dockerfile
    - added helm chart configuration
    - added '/api/health' healtcheck endpoint for liveness/readiness probes
    - updated bitbucket pipelines for k8s deployment

---
+++ pages/api/health.ts
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+): void {
+  res.status(200).send('Astro UI');
+}
