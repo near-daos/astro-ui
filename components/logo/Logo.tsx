5dd3eeea858674e53544da7a350da2a3bdbca94f
Sergii Krupnyk <s.krupnyk@gmail.com>
2021-10-05 07:20:32 +0000

    Merged in fix/CU-1nuezcq-click-astro-to-astrodao.com (pull request #347) Fix/CU-1nuezcq Click on ASTRO logo should take user to astrodao.com landing page

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 import { Icon } from 'components/Icon';
 
@@ -11,12 +10,10 @@ interface LogoProps {
   className?: string;
 }
 
-export const Logo: React.VFC<LogoProps> = ({ className, ...props }) => {
-  const router = useRouter();
-
+export const Logo: React.VFC<LogoProps> = ({ className }) => {
   return (
-    <Link href={{ href: '/dao/[dao]', query: router.query }} passHref>
-      <a href="*" {...props} className={cn(styles.logo, className)}>
+    <Link href="https://astrodao.com/" passHref>
+      <a className={cn(styles.logo, className)}>
         <Icon width={92} name="appLogo" />
       </a>
     </Link>
