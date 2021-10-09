0680f374f56ee1683b4fff3b6b2f289a67306fa1
overzunov <olegverzunov@gmail.com>
2021-10-09 20:35:35 +0000

    feat(polls page): filter polls by status and sho them as tabs

--- pages/dao/[dao]/tasks/polls/PollsPage.tsx
+++ pages/dao/[dao]/tasks/polls/PollsPage.tsx
@@ -1,24 +1,15 @@
-import React, {
-  CSSProperties,
-  FC,
-  useCallback,
-  useEffect,
-  useRef,
-  useState
-} from 'react';
-import { ListOnScrollProps, VariableSizeList } from 'react-window';
-import { useMedia } from 'react-use';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { CreatePollDialog } from 'features/poll/dialogs/create-poll-dialog/CreatePollDialog';
 import { ProposalCardRenderer } from 'components/cards/proposal-card';
 import { useModal } from 'components/modal/hooks';
-import ScrollList from 'components/scroll-list/ScrollList';
 import { Button } from 'components/button/Button';
-import { IconButton } from 'components/button/IconButton';
 import styles from 'pages/dao/[dao]/tasks/polls/polls.module.scss';
 import { SputnikService } from 'services/SputnikService';
 import { Proposal } from 'types/proposal';
+import { ProposalsTabsFilter } from 'components/proposals-tabs-filter';
+import { filterProposalsByStatus } from 'features/dao-home/helpers';
 
 interface PollsPageProps {
   data: Proposal[];
@@ -31,58 +22,20 @@ const PollsPage: FC<PollsPageProps> = () => {
 
   const [showModal] = useModal(CreatePollDialog);
 
-  const [showResetScroll, setShowResetScroll] = useState(false);
-  const scrollListRef = useRef<VariableSizeList>(null);
-  const isMobile = useMedia('(max-width: 767px)');
-
   function fetchPolls(daoId: string) {
     SputnikService.getPolls(daoId).then(res => setPollsList(res));
   }
-
-  const handleScroll = useCallback(({ scrollOffset }: ListOnScrollProps) => {
-    if (scrollOffset > 100) {
-      setShowResetScroll(true);
-    } else {
-      setShowResetScroll(false);
-    }
-  }, []);
 
   const handleCreateClick = useCallback(async () => {
     await showModal();
     fetchPolls(query.dao as string);
   }, [query.dao, showModal]);
 
-  const resetScroll = useCallback(() => {
-    if (!scrollListRef || !scrollListRef.current) {
-      return;
-    }
-
-    scrollListRef.current.scrollToItem(0);
-  }, [scrollListRef]);
-
   useEffect(() => {
     if (query.dao) {
       fetchPolls(query.dao as string);
     }
   }, [query.dao]);
-
-  const renderCard = ({
-    index,
-    style
-  }: {
-    index: number;
-    style: CSSProperties;
-  }) => (
-    <div
-      style={{
-        ...style,
-        marginTop: '0',
-        marginBottom: '16px'
-      }}
-    >
-      <ProposalCardRenderer proposal={pollsList[index]} />
-    </div>
-  );
 
   return (
     <div className={styles.root}>
@@ -93,22 +46,33 @@ const PollsPage: FC<PollsPageProps> = () => {
         </Button>
       </div>
       <div className={styles.polls}>
-        <ScrollList
-          itemCount={pollsList.length}
-          onScroll={handleScroll}
-          height={700}
-          itemSize={() => (isMobile ? 230 : 158)}
-          ref={scrollListRef}
-          renderItem={renderCard}
+        <ProposalsTabsFilter
+          filter={filterProposalsByStatus}
+          proposals={pollsList}
+          tabsConfig={[
+            {
+              label: 'Active proposals',
+              className: styles.activeProposalsTab
+            },
+            {
+              label: 'Approved',
+              className: styles.approvedProposalsTab
+            },
+            {
+              label: 'Failed',
+              className: styles.failedProposalsTab
+            }
+          ]}
+          tabContentRenderer={(proposals: Proposal[]) => (
+            <div className={styles.proposalsView}>
+              {proposals.map(item => (
+                <div className={styles.proposalCardWrapper}>
+                  <ProposalCardRenderer proposal={item} />
+                </div>
+              ))}
+            </div>
+          )}
         />
-        {showResetScroll ? (
-          <IconButton
-            icon="buttonResetScroll"
-            size={isMobile ? 'medium' : 'large'}
-            className={styles.reset}
-            onClick={resetScroll}
-          />
-        ) : null}
       </div>
     </div>
   );
