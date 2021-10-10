70d1f10757d5998f40f754ce9814844772e6dd61
aponomarenko <alexey.a.ponomarenko@gmail.com>
2021-10-11 01:15:53 +0300

    feat(dao card): new dao card design

--- components/cards/dao-card/DaoCard.tsx
+++ components/cards/dao-card/DaoCard.tsx
@@ -1,77 +1,112 @@
 import React from 'react';
-import TextTruncate from 'react-text-truncate';
 import Link from 'next/link';
+import TextTruncate from 'react-text-truncate';
 
-import { FormattedNumericValue } from 'components/cards/components/formatted-numeric-value/FormattedNumericValue';
-import { ImageWithFallback } from 'components/image-with-fallback';
+import { DAO } from 'types/dao';
 
-import styles from 'components/cards/dao-card/dao-card.module.scss';
+import { useNearPrice } from 'hooks/useNearPrice';
 
 import defaultFlag from 'stories/dao-home/assets/flag.png';
 
+import { ImageWithFallback } from 'components/image-with-fallback';
+import { FormattedNumericValue } from 'components/cards/components/formatted-numeric-value/FormattedNumericValue';
+
+import { formatCurrency } from 'utils/formatCurrency';
+
+import styles from 'components/cards/dao-card/dao-card.module.scss';
+
 interface DaoCardProps {
+  dao: DAO;
   title: string;
   flag: string;
   daoAccountName: string;
   description: string | null;
   activeProposals: number;
-  funds: string;
   members: number;
 }
 
+// TODO refactor component to get all data from dao prop
 const DaoCard: React.FC<DaoCardProps> = ({
+  dao,
   title,
   flag,
   daoAccountName,
   description,
   activeProposals,
-  members,
-  funds
+  members
 }) => {
-  const renderFooterItem = (
-    caption: string,
-    value: number | string,
-    suffix?: string
-  ) => (
-    <div className={styles.flexContainer}>
-      <div className={styles.grayCaption}>{caption}</div>
-      <FormattedNumericValue value={value} suffix={suffix} />
-    </div>
-  );
+  const nearPrice = useNearPrice();
+
+  const { funds, proposals } = dao;
+
+  function roundToTwoDigits(num: string) {
+    return Math.round((parseFloat(num) + Number.EPSILON) * 100) / 100;
+  }
+
+  function getUSDFunds() {
+    return formatCurrency(parseFloat(funds) * nearPrice);
+  }
 
   return (
     <Link href={`/dao/${daoAccountName}`} passHref>
       <div className={styles.daoCard}>
-        <div className={styles.iconWrapper}>
-          <ImageWithFallback
-            fallbackSrc={defaultFlag.src}
-            src={flag}
-            width={64}
-            height={64}
-            alt={`${title} Dao Logo`}
-          />
-        </div>
-        <div className={styles.titleCaption}>
-          <h2 className={styles.noMargin}>{title}</h2>
-        </div>
-        <div className={styles.urlCaption}>{daoAccountName}</div>
-        <div className={styles.descriptionCaption}>
-          <TextTruncate
-            line={2}
-            element="span"
-            truncateText="…"
-            text={description ?? ''}
-            textTruncateChild={null}
-          />
-        </div>
-        <div className={styles.activeProposals}>
-          <div className={styles.activeProposalsCaption}>
-            {activeProposals} active proposals
+        <div>
+          <div className={styles.header}>
+            <div className={styles.iconWrapper}>
+              <ImageWithFallback
+                fallbackSrc={defaultFlag.src}
+                src={flag}
+                width={140}
+                height={140}
+                alt={`${title} Dao Logo`}
+              />
+            </div>
+            <div>
+              <h2 className={styles.noMargin}>{title}</h2>
+              <div className={styles.urlCaption}>{daoAccountName}</div>
+            </div>
+          </div>
+          <div className={styles.descriptionCaption}>
+            <TextTruncate
+              line={3}
+              element="span"
+              truncateText="…"
+              text={description ?? ''}
+              textTruncateChild={null}
+            />
           </div>
         </div>
-        <div className={styles.membersAndFunds}>
-          {renderFooterItem('Members', members)}
-          {renderFooterItem('Funds', funds, 'usd')}
+        <div>
+          <div className={styles.membersAndFunds}>
+            <div className={styles.membersAndFundsContainer}>
+              <div className={styles.grayCaption}>Members</div>
+              <FormattedNumericValue value={members} />
+            </div>
+            <div className={styles.membersAndFundsContainer}>
+              <div className={styles.grayCaption}>Funds</div>
+              <FormattedNumericValue
+                suffix="NEAR"
+                value={roundToTwoDigits(funds)}
+                valueClassName={styles.nearFundsValue}
+                suffixClassName={styles.nearFundsSuffix}
+              />
+              <FormattedNumericValue
+                suffix="usd"
+                value={getUSDFunds()}
+                valueClassName={styles.usdFundsValue}
+                suffixClassName={styles.usdFundsSuffix}
+              />
+            </div>
+          </div>
+          <div className={styles.delimiter} />
+          <div className={styles.activeProposals}>
+            <span className={styles.activeProposalsNumber}>
+              {activeProposals}&nbsp;
+            </span>
+            active proposals
+          </div>
+          <div className={styles.totalProposals}>{proposals} in total</div>
+          <div className={styles.createProposal}>Create Proposal</div>
         </div>
       </div>
     </Link>
