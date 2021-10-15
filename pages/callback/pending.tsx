75fac0bfa2d0f3e5c16caf06cfc8fb29fbffcad7
overzunov <olegverzunov@gmail.com>
2021-10-15 12:09:11 +0000

    fix(pending): update urls and fix an error on pending page. add loading

--- pages/callback/pending.tsx
+++ pages/callback/pending.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 import { Loader } from 'components/loader';
 
 const Pending: NextPage = () => {
-  return <Loader title="Uploading data to the wallet" />;
+  return <Loader title="Connecting to the wallet" />;
 };
 
 export default Pending;
