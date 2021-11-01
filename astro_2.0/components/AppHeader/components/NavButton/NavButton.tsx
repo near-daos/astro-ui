2f945c820542b38b9a241e1c5634c07082ee6ddf
aponomarenko <alexey.a.ponomarenko@gmail.com>
2021-11-01 23:11:10 +0200

    feat(app header): header updates

--- astro_2.0/components/AppHeader/components/NavButton/NavButton.tsx
+++ astro_2.0/components/AppHeader/components/NavButton/NavButton.tsx
@@ -4,15 +4,16 @@ import React, { FC } from 'react';
 
 import { useIsHrefActive } from 'hooks/useIsHrefActive';
 
-import { Icon } from 'components/Icon';
+import { Icon, IconName } from 'components/Icon';
 
 import styles from './NavButton.module.scss';
 
 interface NavButtonProps {
   href: string;
+  icon: IconName;
 }
 
-export const NavButton: FC<NavButtonProps> = ({ href, children }) => {
+export const NavButton: FC<NavButtonProps> = ({ icon, href, children }) => {
   const isActive = useIsHrefActive(href);
 
   const rootClassName = cn(styles.root, {
@@ -23,7 +24,7 @@ export const NavButton: FC<NavButtonProps> = ({ href, children }) => {
     <Link href={href} passHref>
       <div className={rootClassName}>
         <div className={styles.description}>
-          <Icon height={16} name="stateCommunities" className={styles.icon} />
+          <Icon height={16} name={icon} className={styles.icon} />
           <div>{children}</div>
         </div>
         <div className={styles.underline} />
--- astro_2.0/components/AppHeader/components/SearchBar/components/DropdownResults/components/ResultSection/components/SearchResultProposalLine/SearchResultProposalLine.tsx
+++ astro_2.0/components/AppHeader/components/SearchBar/components/DropdownResults/components/ResultSection/components/SearchResultProposalLine/SearchResultProposalLine.tsx
@@ -1,6 +1,9 @@
-import { VFC } from 'react';
+import isEmpty from 'lodash/isEmpty';
+import { ReactNode, VFC } from 'react';
 
 import { Proposal } from 'types/proposal';
+
+import { useSearchResults } from 'features/search/search-results';
 
 import styles from './SearchResultProposalLine.module.scss';
 
@@ -13,7 +16,43 @@ export const SearchResultProposalLine: VFC<SearchResultProposalLineProps> = ({
   data,
   onClick,
 }) => {
+  const { searchResults } = useSearchResults();
+
+  const query = searchResults?.query || '';
   const { description } = data;
+
+  const reg = new RegExp(query, 'gi');
+  const matchingParts = description.match(reg) || [];
+
+  // Highlights search entries in a string
+  const result = isEmpty(matchingParts)
+    ? { res: description }
+    : matchingParts.reduce(
+        (acc: { str: string; res: ReactNode[] }, part, index) => {
+          const partIndex = acc.str.indexOf(part);
+
+          if (partIndex > 0) {
+            const portion = acc.str.slice(0, partIndex);
+
+            acc.res.push(portion);
+            acc.str = acc.str.replace(portion, '');
+          }
+
+          // eslint-disable-next-line react/no-array-index-key
+          acc.res.push(<b key={`part-${index}`}>{part}</b>);
+          acc.str = acc.str.replace(part, '');
+
+          if (index === matchingParts.length - 1) {
+            acc.res.push(acc.str);
+          }
+
+          return acc;
+        },
+        {
+          str: description,
+          res: [],
+        }
+      );
 
   return (
     <div
@@ -23,7 +62,7 @@ export const SearchResultProposalLine: VFC<SearchResultProposalLineProps> = ({
       onKeyPress={onClick}
       className={styles.root}
     >
-      {description}
+      {result.res}
     </div>
   );
 };
