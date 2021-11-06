65efc22db7a55d7423b45bf4dd5c91e623ee91a1
denis-krasovskyi <denis.krasovskyi@gmail.com>
2021-11-06 13:20:13 +0200

    feat: update polls page

--- pages/dao/[dao]/tasks/polls/PollsPage.tsx
+++ pages/dao/[dao]/tasks/polls/PollsPage.tsx
@@ -1,93 +1,243 @@
-import isEmpty from 'lodash/isEmpty';
 import { useRouter } from 'next/router';
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 
-import { CreatePollDialog } from 'features/poll/dialogs/create-poll-dialog/CreatePollDialog';
-import { ProposalCardRenderer } from 'components/cards/proposal-card';
-import { useModal } from 'components/modal/hooks';
 import { Button } from 'components/button/Button';
 import styles from 'pages/dao/[dao]/tasks/polls/polls.module.scss';
-import { SputnikHttpService } from 'services/sputnik';
-import { Proposal } from 'types/proposal';
-import { ProposalsTabsFilter } from 'components/proposals-tabs-filter';
-import { filterProposalsByStatus } from 'features/dao-home/helpers';
+import {
+  FeedCategories,
+  Proposal,
+  ProposalStatuses,
+  ProposalVariant,
+} from 'types/proposal';
 import { useAuthContext } from 'context/AuthContext';
 import { NoResultsView } from 'features/no-results-view';
+import { BreadCrumbs } from 'astro_2.0/components/BreadCrumbs';
+import NavLink from 'astro_2.0/components/NavLink';
+import { DaoDetailsMinimized } from 'astro_2.0/components/DaoDetails';
+import { CreateProposal } from 'astro_2.0/features/CreateProposal';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { ProposalCard } from 'astro_2.0/components/ProposalCardRenderer/components/ProposalCard';
+import { getVoteDetails } from 'features/vote-policy/helpers';
+import { getScope } from 'components/cards/expanded-proposal-card/helpers';
+import { LetterHeadWidget } from 'astro_2.0/components/ProposalCardRenderer/components/LetterHeadWidget';
+import { DAO } from 'types/dao';
+import { PaginationResponse } from 'types/api';
+import { Loader } from 'components/loader';
+import { LIST_LIMIT_DEFAULT } from 'services/sputnik/constants';
+import { SputnikHttpService } from 'services/sputnik/SputnikHttpService';
+import { useAsyncFn, useUpdateEffect } from 'react-use';
+import { ProposalCardRenderer } from 'astro_2.0/components/ProposalCardRenderer';
+import StatusFilters from 'astro_2.0/components/Feed/StatusFilters';
+import { ProposalsQueries } from 'services/sputnik/types/proposals';
 
-interface PollsPageProps {
-  data: Proposal[];
+export interface PollsPageProps {
+  dao: DAO;
+  initialPollsData: PaginationResponse<Proposal[]>;
 }
 
-const PollsPage: FC<PollsPageProps> = () => {
-  const { query } = useRouter();
-  const { accountId, login } = useAuthContext();
+const PollsPage: FC<PollsPageProps> = ({ dao, initialPollsData }) => {
+  const router = useRouter();
+  const { query } = router;
+  const daoId = query.dao as string;
+  const status = query.status as ProposalStatuses;
 
-  const [pollsList, setPollsList] = useState<Proposal[]>([]);
-
-  const [showModal] = useModal(CreatePollDialog);
-
-  function fetchPolls(daoId: string) {
-    SputnikHttpService.getPolls(daoId).then(res => setPollsList(res));
-  }
-
-  const showCreatePollDialog = useCallback(async () => {
-    await showModal();
-    fetchPolls(query.dao as string);
-  }, [query.dao, showModal]);
-
-  useEffect(() => {
-    if (query.dao) {
-      fetchPolls(query.dao as string);
-    }
-  }, [query.dao]);
-
-  const handleCreateClick = useCallback(
-    () => (accountId ? showCreatePollDialog() : login()),
-    [login, showCreatePollDialog, accountId]
+  const { accountId } = useAuthContext();
+  const [showCreateProposal, setShowCreateProposal] = useState(false);
+  const [pollsData, setPollsData] = useState<PaginationResponse<Proposal[]>>(
+    initialPollsData
   );
+
+  const [fetchPollsState, fetchPollsData] = useAsyncFn(
+    async (prevState?: PaginationResponse<Proposal[]>) => {
+      const response = await SputnikHttpService.getProposalsList({
+        daoViewFilter: dao.name,
+        offset: prevState?.data.length || 0,
+        limit: LIST_LIMIT_DEFAULT,
+        category: FeedCategories.Polls,
+        status,
+        daoFilter: 'All DAOs',
+      });
+
+      return {
+        ...response,
+        data: [...(prevState?.data || []), ...response.data],
+      };
+    },
+    [pollsData.data.length, status, daoId]
+  );
+
+  useUpdateEffect(() => {
+    fetchPollsData().then(async () => setPollsData(await fetchPollsData()));
+  }, [daoId, status, pollsData.data.length]);
+
+  const handleCreateProposal = useCallback(() => {
+    setShowCreateProposal(true);
+  }, []);
+
+  const loadMore = async () => {
+    if (fetchPollsState.loading) {
+      return;
+    }
+
+    await fetchPollsData(pollsData);
+  };
+
+  const onProposalFilterChange = (value?: string) => async () => {
+    const nextQuery = {
+      ...router.query,
+      status: value,
+    } as ProposalsQueries;
+
+    if (!value) {
+      delete nextQuery.status;
+    }
+
+    await router.replace(
+      {
+        query: nextQuery,
+      },
+      undefined,
+      { shallow: true, scroll: false }
+    );
+  };
+
+  const isEmptyList = (pollsData.data || []).length === 0;
 
   return (
     <div className={styles.root}>
+      <BreadCrumbs className={styles.breadcrumbs}>
+        <NavLink href="/all/daos">All DAOs</NavLink>
+        <NavLink href={`/dao/${daoId}`}>{dao?.displayName || dao?.id}</NavLink>
+        <span>Polls</span>
+      </BreadCrumbs>
+      <div className={styles.dao}>
+        <DaoDetailsMinimized
+          dao={dao}
+          accountId={accountId}
+          onCreateProposalClick={handleCreateProposal}
+        />
+        {showCreateProposal && (
+          <div className={styles.newProposalWrapper}>
+            <CreateProposal
+              dao={dao}
+              proposalVariant={ProposalVariant.ProposePoll}
+              onCreate={isSuccess => {
+                if (isSuccess) {
+                  setShowCreateProposal(false);
+                  fetchPollsData();
+                }
+              }}
+              onClose={() => {
+                setShowCreateProposal(false);
+              }}
+            />
+          </div>
+        )}
+      </div>
       <div className={styles.header}>
         <h1>Polls</h1>
-        <Button variant="black" size="small" onClick={handleCreateClick}>
+        <Button variant="black" size="small" onClick={handleCreateProposal}>
           Create new poll
         </Button>
       </div>
-      <div className={styles.polls}>
-        <ProposalsTabsFilter
-          filter={filterProposalsByStatus}
-          proposals={pollsList}
-          tabsConfig={[
+      <div className={styles.statusFilterWrapper}>
+        <StatusFilters
+          proposal={status}
+          onChange={onProposalFilterChange}
+          list={[
+            { value: undefined, label: 'All', name: 'All' },
             {
-              label: 'Active proposals',
-              className: styles.activeProposalsTab,
+              value: ProposalStatuses.Active,
+              label: 'Active',
+              name: ProposalStatuses.Active,
             },
             {
+              value: ProposalStatuses.Approved,
               label: 'Approved',
-              className: styles.approvedProposalsTab,
+              name: ProposalStatuses.Approved,
+              classes: {
+                inputWrapperChecked:
+                  styles.categoriesListApprovedInputWrapperChecked,
+              },
             },
             {
+              value: ProposalStatuses.Failed,
               label: 'Failed',
-              className: styles.failedProposalsTab,
+              name: ProposalStatuses.Failed,
+              classes: {
+                inputWrapperChecked:
+                  styles.categoriesListFailedInputWrapperChecked,
+              },
             },
           ]}
-          tabContentRenderer={(proposals: Proposal[]) => {
-            if (isEmpty(proposals)) {
-              return <NoResultsView title="No proposals here" />;
-            }
-
-            return (
-              <div className={styles.proposalsView}>
-                {proposals.map(item => (
-                  <div className={styles.proposalCardWrapper}>
-                    <ProposalCardRenderer proposal={item} />
-                  </div>
-                ))}
-              </div>
-            );
-          }}
+          className={styles.statusFilterRoot}
         />
+      </div>
+      <div className={styles.polls}>
+        <InfiniteScroll
+          dataLength={pollsData?.data.length || 0}
+          next={loadMore}
+          hasMore={(pollsData.data.length || 0) < (pollsData.total || 0)}
+          loader={<h4 className={styles.loading}>Loading...</h4>}
+          style={{ overflow: 'initial' }}
+          endMessage={
+            !isEmptyList &&
+            pollsData.data.length > LIST_LIMIT_DEFAULT && (
+              <p className={styles.loading}>
+                <b>You have seen it all</b>
+              </p>
+            )
+          }
+        >
+          {(pollsData?.data || []).map(item => {
+            return (
+              <ProposalCardRenderer
+                key={`${item.id}${item.proposalId}`}
+                proposalCardNode={
+                  <ProposalCard
+                    type={item.kind.type}
+                    status={item.status}
+                    proposer={item.proposer}
+                    description={item.description}
+                    link={item.link}
+                    proposalId={item.proposalId}
+                    proposalTxHash={item.txHash}
+                    accountId={accountId}
+                    dao={item.dao}
+                    likes={item.voteYes}
+                    dislikes={item.voteNo}
+                    liked={item.votes[accountId] === 'Yes'}
+                    disliked={item.votes[accountId] === 'No'}
+                    voteDetails={
+                      item.dao.policy.defaultVotePolicy.ratio
+                        ? getVoteDetails(
+                            item.dao,
+                            getScope(item.kind.type),
+                            item
+                          ).details
+                        : undefined
+                    }
+                    content={null}
+                  />
+                }
+                letterHeadNode={
+                  <LetterHeadWidget
+                    type={item.kind.type}
+                    // TODO replace the link with supposed one
+                    coverUrl="/cover.png"
+                  />
+                }
+                className={styles.proposalCardWrapper}
+              />
+            );
+          })}
+
+          {isEmptyList && fetchPollsState.loading && <Loader />}
+
+          {isEmptyList && !fetchPollsState.loading && (
+            <NoResultsView title="No proposals here" />
+          )}
+        </InfiniteScroll>
       </div>
     </div>
   );
