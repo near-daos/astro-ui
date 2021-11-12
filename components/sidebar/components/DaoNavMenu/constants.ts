f1c180a3ea10d3b61e3efce12135d8bef142f5e9
aponomarenko <alexey.a.ponomarenko@gmail.com>
2021-11-13 01:53:08 +0200

    feat(refactor): partially removed redundant code

--- components/sidebar/components/DaoNavMenu/constants.ts
+++
@@ -1,4 +0,0 @@
-export const TASKS_SECTION_ID = 'tasks';
-export const TREASURY_SECTION_ID = 'treasury';
-export const GROUPS_SECTION_ID = 'groups';
-export const GOVERNANCE_SECTION_ID = 'governance';
