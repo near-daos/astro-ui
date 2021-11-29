5afcde9efc229d56c259fb47f268cc964a0cd951
AlexeyPonomarenko <alexey.a.ponomarenko@gmail.com>
2021-11-29 23:07:17 +0200

    feat(create proposal): removed duplicates from list of groups

--- astro_2.0/features/CreateProposal/components/AddMemberToGroupContent/AddMemberToGroupContent.tsx
+++ astro_2.0/features/CreateProposal/components/AddMemberToGroupContent/AddMemberToGroupContent.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
 import cn from 'classnames';
+import React, { FC } from 'react';
 import { useFormContext } from 'react-hook-form';
+
 import { Input } from 'components/inputs/input/Input';
-import { DropdownSelect } from 'components/inputs/select/DropdownSelect';
 import { Group } from 'features/vote-policy/components/group';
+import { DropdownSelect } from 'components/inputs/select/DropdownSelect';
 import { InputWrapper } from 'astro_2.0/features/CreateProposal/components/InputWrapper';
 
 import styles from './AddMemberToGroupContent.module.scss';
