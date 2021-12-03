b127c8c69dced36ff957678f4e1e74cfb2d94511
unholywhite <s.krupnyk@gmail.com>
2021-12-03 04:32:27 +0200

    feat: notification card

---
+++ types/notification.ts
@@ -0,0 +1,18 @@
+export enum NotificationType {
+  DaoConfig = 'DaoConfig',
+  Bounty = 'Bounty',
+  Polls = 'Polls',
+  Transfer = 'Transfer',
+  AddMember = 'AddMember',
+  RemoveMember = 'RemoveMember',
+  Governance = 'Governance',
+}
+
+export enum NotificationStatus {
+  Default = 'Default',
+  Approved = 'Approved',
+  Rejected = 'Rejected',
+  Success = 'Success',
+  Alert = 'Alert',
+  EndingSoon = 'EndingSoon',
+}
