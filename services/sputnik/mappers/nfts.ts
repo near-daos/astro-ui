091d776493184ed3e6813f1f7c6f4ec122b96147
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2021-12-04 14:16:28 +0200

    fix(nfts page): fix nfts page data display, add nft card preloader (#133)
    * fix(nfts page): fix nfts page data display, add nft card preloader

---
+++ services/sputnik/mappers/nfts.ts
@@ -0,0 +1,49 @@
+import { NftToken, NftTokenResponse } from 'types/token';
+
+export function mapNftTokenResponseToNftToken(
+  data: NftTokenResponse[]
+): NftToken[] {
+  return data
+    .reduce<NftToken[]>((res, item) => {
+      const { id, baseUri, metadata, contract } = item;
+
+      if (!metadata) return res;
+
+      const { media, reference } = metadata;
+
+      const isMediaContainsUrl = media?.indexOf('http') === 0;
+
+      let uri;
+      let isExternalReference = false;
+
+      if (baseUri && media && !isMediaContainsUrl) {
+        uri = `${baseUri}/${media}`;
+      } else if (isMediaContainsUrl) {
+        uri = media;
+      } else if (contract?.baseUri && !media && reference) {
+        uri = `${contract?.baseUri}/${reference}`;
+        isExternalReference = true;
+      } else if (media) {
+        uri = `https://cloudflare-ipfs.com/ipfs/${media}`;
+      }
+
+      if (uri) {
+        res.push({
+          id,
+          uri,
+          description: metadata.description ?? null,
+          title: metadata.title ?? null,
+          isExternalReference,
+        });
+      }
+
+      return res;
+    }, [])
+    .sort((a, b) => {
+      if (a.uri > b.uri) return 1;
+
+      if (a.uri < b.uri) return -1;
+
+      return 0;
+    });
+}
