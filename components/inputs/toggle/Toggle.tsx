12d301650e5bcb675ed9f46c57a1b783f39dffdb
unholywhite <s.krupnyk@gmail.com>
2021-12-07 00:47:19 +0200

    fix: notification card

--- components/inputs/toggle/Toggle.tsx
+++ components/inputs/toggle/Toggle.tsx
@@ -8,17 +8,28 @@ import styles from './toggle.module.scss';
 
 export interface CheckboxProps extends React.HTMLProps<HTMLInputElement> {
   label?: string | undefined;
+  mobileListView?: boolean;
+  groupSwitch?: boolean;
 }
 
 export const Toggle = React.forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ label, className, onChange, ...props }, externalRef) => {
+  (
+    { label, mobileListView, groupSwitch, className, onChange, ...props },
+    externalRef
+  ) => {
     const id = useId(props.id);
     const [checked, setChecked] = useState(props.defaultChecked);
 
     const value = props.checked != null ? props.checked : checked;
 
+    const rootClassName = cn(styles.root, {
+      [styles.mobileList]: mobileListView,
+      [styles.groupSwitch]: groupSwitch,
+      className,
+    });
+
     return (
-      <label htmlFor={id} className={cn(styles.root, className)}>
+      <label htmlFor={id} className={rootClassName}>
         <input
           id={id}
           {...props}
@@ -33,7 +44,13 @@ export const Toggle = React.forwardRef<HTMLInputElement, CheckboxProps>(
           }}
         />
         <ToggleDisplay value={value} />
-        {label}
+        <span
+          className={cn(styles.labelText, {
+            [styles.on]: value,
+          })}
+        >
+          {groupSwitch ? 'On' : label}
+        </span>
       </label>
     );
   }
