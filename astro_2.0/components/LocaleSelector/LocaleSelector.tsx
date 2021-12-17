6578ad6ae27e446bef98305ca77bbb3dc5a5876d
Overzunov <overzunov@lohika.com>
2021-12-17 11:34:26 +0200

    feat(locale selector): implement locale selector component according to latest ui

--- astro_2.0/components/LocaleSelector/LocaleSelector.tsx
+++ astro_2.0/components/LocaleSelector/LocaleSelector.tsx
@@ -1,28 +1,52 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
+import cn from 'classnames';
+import { CircleFlag } from 'react-circle-flags';
 
-import { Dropdown } from 'components/Dropdown';
+import { GenericDropdown } from 'astro_2.0/components/GenericDropdown';
+import { Locales } from 'astro_2.0/components/LocaleSelector/components/Locales';
 
 import styles from './LocaleSelector.module.scss';
 
 export const LocaleSelector: FC = () => {
   const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const rootRef = useRef(null);
+
+  const locale = router.locale ?? 'en';
 
   return (
-    <div className={styles.root}>
-      <Dropdown
-        className={styles.select}
-        value={router.locale}
-        onChange={v => {
-          router.push(router.asPath, router.asPath, { locale: v });
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+    <div className={styles.root} onClick={e => e.stopPropagation()}>
+      <GenericDropdown
+        arrow
+        arrowClassName={styles.arrow}
+        isOpen={open}
+        onOpenUpdate={setOpen}
+        options={{
+          modifiers: [
+            {
+              name: 'offset',
+              options: {
+                offset: [0, 24],
+              },
+            },
+          ],
         }}
-        options={
-          router.locales?.map(item => ({
-            label: item,
-            value: item,
-          })) ?? []
+        parent={
+          <div>
+            <div className={cn(styles.selector)} ref={rootRef}>
+              <CircleFlag
+                countryCode={locale === 'en' ? 'us' : locale}
+                className={styles.flag}
+              />{' '}
+              {locale}
+            </div>
+          </div>
         }
-      />
+      >
+        <Locales />
+      </GenericDropdown>
     </div>
   );
 };
