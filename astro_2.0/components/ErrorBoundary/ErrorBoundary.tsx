c503d431cf8299df03a8a976ac3150b585832d34
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2021-12-22 11:21:55 +0200

    Fix/view proposal (#239)
    * fix(view proposal): fix metadata parsing, add error handling

---
+++ astro_2.0/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,77 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+
+// Styles
+import styles from './ErrorBoundary.module.scss';
+
+// Types
+type ErrorBoundaryProps = {
+  children: ReactNode;
+  hidden?: boolean;
+  message?: string;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+  error?: string;
+};
+
+const DEFAULT_MESSAGE = 'Error rendering component';
+
+/**
+ * Error boundary wrapper
+ */
+class ErrorBoundary extends React.PureComponent<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  /**
+   * Update state with error details
+   */
+  static getDerivedStateFromError(
+    error: Error
+  ): {
+    hasError: boolean;
+    error: Error;
+  } {
+    return { hasError: true, error };
+  }
+
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    hasError: false,
+    error: undefined,
+  };
+
+  /**
+   * Log error
+   */
+  componentDidCatch(error: Error, info: ErrorInfo): void {
+    // Todo - log the error to an error reporting service
+    // eslint-disable-next-line
+    console.log(error, info, this.state.error);
+  }
+
+  /**
+   * Render fallback UI
+   */
+  render(): ReactNode {
+    const { hasError } = this.state;
+    const { hidden, message, children } = this.props;
+
+    if (hasError && hidden) {
+      return null;
+    }
+
+    if (hasError) {
+      return (
+        <div className={styles.message} title={message || DEFAULT_MESSAGE}>
+          {message || DEFAULT_MESSAGE}
+        </div>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
