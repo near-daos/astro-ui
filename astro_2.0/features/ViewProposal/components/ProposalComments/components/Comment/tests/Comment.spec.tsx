677334206dac9b9ede602083d09843651e0185db
Overzunov <overzunov@lohika.com>
2022-01-10 20:43:39 +0200

    feat(comment): parse links and show images in comments

---
+++ astro_2.0/features/ViewProposal/components/ProposalComments/components/Comment/tests/Comment.spec.tsx
@@ -0,0 +1,27 @@
+import { render } from 'jest/testUtils';
+
+import { Comment } from 'astro_2.0/features/ViewProposal/components/ProposalComments/components/Comment';
+
+const onDelete = jest.fn();
+const onReport = jest.fn();
+
+describe('App Footer', () => {
+  it('Should render Comment component', () => {
+    const { container } = render(
+      <Comment
+        accountId="james.testnet"
+        commentId={1}
+        isMyComment
+        createdAt="2022-01-10T18:39:04.672Z"
+        isCouncilUser={false}
+        isReported={false}
+        onDelete={onDelete}
+        onReport={onReport}
+        reportsCount={1}
+        message="My name is Bond. James bond."
+      />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
