c1c51262a0febb3838d978be4aa437d7f6a98c48
AlexeyPonomarenko <alexey.a.ponomarenko@gmail.com>
2022-01-25 12:40:10 +0200

    refactor(to base 64): removed redundant code (#465)

--- utils/jsonToBase64Str.ts
+++ utils/jsonToBase64Str.ts
@@ -1,4 +1,3 @@
-// TODO check code and replace redundant methods that only call this method.
 export function jsonToBase64Str(data: Record<string, unknown>): string {
   return Buffer.from(JSON.stringify(data)).toString('base64');
 }
