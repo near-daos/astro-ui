66c7d65d59df303e9e9ec74ab8889ddd23164179
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2022-02-07 11:24:27 +0200

    fix(dao nested pages): fix mobile view for nfts, settings and polls pages (#591)
    Co-authored-by: Overzunov <overzunov@lohika.com>

--- pages/dao/[dao]/governance/settings/SettingsPage.tsx
+++ pages/dao/[dao]/governance/settings/SettingsPage.tsx
@@ -1,288 +1,39 @@
-import React, { useCallback } from 'react';
+import React, { useMemo } from 'react';
 import { NextPage } from 'next';
-import classNames from 'classnames';
-import { useRouter } from 'next/router';
-import TextTruncate from 'react-text-truncate';
-
-import { ALL_DAOS_URL } from 'constants/routing';
-
-import { Icon } from 'components/Icon';
-import { Badge } from 'components/badge/Badge';
-import { NavLink } from 'astro_2.0/components/NavLink';
-import { BreadCrumbs } from 'astro_2.0/components/BreadCrumbs';
-import { DaoSetting } from 'astro_2.0/features/DaoGovernance/components/DaoSetting';
-import { SettingsCard } from 'astro_2.0/features/DaoGovernance/components/SettingsCard';
-import { InfoValue } from 'astro_2.0/components/InfoBlockWidget/components/InfoValue';
-import { DefaultVotingPolicy } from 'astro_2.0/components/DefaultVotingPolicy';
-import { PolicyAffectedWarning } from 'astro_2.0/components/PolicyAffectedWarning';
-import { useCreateProposal } from 'astro_2.0/features/CreateProposal/hooks';
-import { FlagPreview } from 'astro_2.0/features/CreateDao/components/FlagPreview/FlagPreview';
-import { SettingsFilterToggle } from 'astro_2.0/features/DaoGovernance/components/SettingsFilterToggle';
-import { getBadgeVariant } from 'features/proposal/helpers';
-import { formatYoktoValue } from 'utils/format';
-import { nanosToDays } from 'astro_2.0/features/DaoGovernance/helper';
-import { useDaoCustomTokens } from 'hooks/useCustomTokens';
 
 import { ProposalVariant } from 'types/proposal';
-
-import { DaoDetailsMinimized } from 'astro_2.0/components/DaoDetails';
 import { DaoContext } from 'types/context';
-import styles from './SettingsPage.module.scss';
+
+import { NestedDaoPageWrapper } from 'astro_2.0/features/pages/nestedDaoPagesContent/NestedDaoPageWrapper';
+import { useGetBreadcrumbsConfig } from 'hooks/useGetBreadcrumbsConfig';
+import { SettingsPageContent } from 'astro_2.0/features/pages/nestedDaoPagesContent/SettingsPageContent';
 
 export interface SettingsPageProps {
   daoContext: DaoContext;
 }
 
-const SettingsPage: NextPage<SettingsPageProps> = ({
-  daoContext: { dao, policyAffectsProposals, userPermissions },
-}) => {
-  const router = useRouter();
-  const { daoFilter } = router.query;
-  const { tokens } = useDaoCustomTokens();
-  const [CreateProposal, toggleCreateProposal] = useCreateProposal();
-
-  const createProposalHandler = useCallback(
-    (proposalVariant: ProposalVariant) => () =>
-      toggleCreateProposal({ proposalVariant }),
-    [toggleCreateProposal]
+const SettingsPage: NextPage<SettingsPageProps> = ({ daoContext }) => {
+  const breadcrumbsConfig = useGetBreadcrumbsConfig(
+    daoContext.dao.id,
+    daoContext.dao.displayName
   );
 
-  const proposalPeriod = nanosToDays(dao.policy.proposalPeriod);
-  const bountyPeriod = nanosToDays(dao.policy.bountyForgivenessPeriod);
+  const breadcrumbs = useMemo(() => {
+    return [
+      breadcrumbsConfig.ALL_DAOS_URL,
+      breadcrumbsConfig.SINGLE_DAO_PAGE,
+      breadcrumbsConfig.SETTINGS,
+    ];
+  }, [breadcrumbsConfig]);
 
   return (
-    <div className={styles.root}>
-      <BreadCrumbs className={styles.breadcrumbsRow}>
-        <NavLink href={ALL_DAOS_URL}>All DAOs</NavLink>
-        <NavLink href={`/dao/${dao?.id}`}>
-          {dao?.displayName || dao?.id}
-        </NavLink>
-        <span>DAO settings</span>
-      </BreadCrumbs>
-
-      <div className={styles.dao}>
-        <DaoDetailsMinimized
-          dao={dao}
-          userPermissions={userPermissions}
-          onCreateProposalClick={() =>
-            toggleCreateProposal({
-              proposalVariant: ProposalVariant.ProposeChangeBonds,
-            })
-          }
-        />
-        <CreateProposal
-          className={styles.createProposal}
-          dao={dao}
-          userPermissions={userPermissions}
-          key={Object.keys(tokens).length}
-          daoTokens={tokens}
-          showFlag={false}
-          onClose={toggleCreateProposal}
-        />
-        <PolicyAffectedWarning
-          data={policyAffectsProposals}
-          className={styles.warningWrapper}
-        />
-      </div>
-
-      <div className={styles.titleRow}>DAO settings</div>
-
-      <div className={styles.sideFilter}>
-        <SettingsFilterToggle />
-      </div>
-
-      <div className={styles.content}>
-        {daoFilter === 'nameAndPurpose' && (
-          <DaoSetting
-            settingsName="Name and Purpose"
-            className={styles.contentRow}
-            disableNewProposal={
-              !userPermissions.isCanCreateProposals ||
-              !userPermissions.isCanCreatePolicyProposals
-            }
-            settingsChangeHandler={createProposalHandler(
-              ProposalVariant.ProposeChangeDaoLegalInfo
-            )}
-          >
-            <div className={styles.row}>
-              <div className={styles.label}>DAO name:</div>
-              <div className={styles.daoName}>{dao.displayName ?? dao.id}</div>
-            </div>
-            <div className={styles.row}>
-              <div className={styles.label}>Purpose:</div>
-              <div className={styles.daoPurpose}>{dao.description}</div>
-            </div>
-          </DaoSetting>
-        )}
-        {daoFilter === 'legalStatusAndDoc' && (
-          <DaoSetting
-            settingsName="Legal Status and doc"
-            className={styles.contentRow}
-            disableNewProposal={
-              !userPermissions.isCanCreateProposals ||
-              !userPermissions.isCanCreatePolicyProposals
-            }
-            settingsChangeHandler={createProposalHandler(
-              ProposalVariant.ProposeChangeDaoLegalInfo
-            )}
-          >
-            <a
-              href={dao.legal?.legalLink ?? ''}
-              target="_blank"
-              rel="noreferrer"
-              className={styles.legalLink}
-            >
-              {dao.legal?.legalStatus || 'Public Limited Company'}
-              <Icon
-                name="buttonExternal"
-                width={14}
-                className={styles.legalIcon}
-              />
-            </a>
-          </DaoSetting>
-        )}
-        {daoFilter === 'links' && (
-          <DaoSetting
-            settingsName="Links"
-            className={styles.contentRow}
-            disableNewProposal={
-              !userPermissions.isCanCreateProposals ||
-              !userPermissions.isCanCreatePolicyProposals
-            }
-            settingsChangeHandler={createProposalHandler(
-              ProposalVariant.ProposeChangeDaoLinks
-            )}
-          >
-            {dao.links.map(link => (
-              <div
-                key={link}
-                className={classNames(
-                  styles.container,
-                  styles.row,
-                  styles.link
-                )}
-              >
-                <Icon name="socialAnyUrl" className={styles.linkIcon} />
-                <TextTruncate
-                  line={1}
-                  element="div"
-                  containerClassName={styles.linkText}
-                  truncateText="…"
-                  text={link}
-                  textTruncateChild={null}
-                />
-              </div>
-            ))}
-          </DaoSetting>
-        )}
-        {daoFilter === 'flagAndLogo' && (
-          <DaoSetting
-            settingsName="Your Flag and Logo"
-            className={styles.contentRow}
-            disableNewProposal={
-              !userPermissions.isCanCreateProposals ||
-              !userPermissions.isCanCreatePolicyProposals
-            }
-            settingsChangeHandler={createProposalHandler(
-              ProposalVariant.ProposeChangeDaoFlag
-            )}
-          >
-            {dao.flagCover && dao.flagLogo && (
-              <FlagPreview logoFile={dao.flagLogo} coverFile={dao.flagCover} />
-            )}
-          </DaoSetting>
-        )}
-        {daoFilter === 'bondAndDeadlines' && (
-          <DaoSetting
-            settingsName="Bond and deadlines"
-            className={styles.contentRow}
-            disableNewProposal={
-              !userPermissions.isCanCreateProposals ||
-              !userPermissions.isCanCreatePolicyProposals
-            }
-            settingsChangeHandler={createProposalHandler(
-              ProposalVariant.ProposeChangeBonds
-            )}
-          >
-            <div className={styles.settingsContainer}>
-              <SettingsCard
-                className={styles.settingsCard}
-                settingName="Proposals"
-                settings={[
-                  {
-                    label: 'Bond to create a proposal',
-                    value: (
-                      <InfoValue
-                        value={formatYoktoValue(dao.policy.proposalBond, 24)}
-                        label="NEAR"
-                      />
-                    ),
-                  },
-                  {
-                    label: 'Time before proposal expires',
-                    value: (
-                      <InfoValue value={proposalPeriod.join(' ')} label="" />
-                    ),
-                  },
-                ]}
-              />
-              <SettingsCard
-                className={styles.settingsCard}
-                settingName="Bounties"
-                settings={[
-                  {
-                    label: 'Bond to claim a bounty',
-                    value: (
-                      <InfoValue
-                        value={formatYoktoValue(dao.policy.bountyBond, 24)}
-                        label="NEAR"
-                      />
-                    ),
-                  },
-                  {
-                    label: 'Time to unclaim a bounty without penalty',
-                    value: (
-                      <InfoValue value={bountyPeriod.join(' ')} label="" />
-                    ),
-                  },
-                ]}
-              />
-            </div>
-          </DaoSetting>
-        )}
-        {daoFilter === 'votingPolicy' && (
-          <DaoSetting
-            settingsName="Voting policy"
-            className={styles.contentRow}
-            disableNewProposal={
-              !userPermissions.isCanCreateProposals ||
-              !userPermissions.isCanCreatePolicyProposals
-            }
-            settingsChangeHandler={createProposalHandler(
-              ProposalVariant.ProposeChangeVotingPolicy
-            )}
-          >
-            <div className={styles.groupsWrapper}>
-              {dao.groups.map(group => {
-                return (
-                  <Badge
-                    size="small"
-                    key={group.slug}
-                    variant={getBadgeVariant(group.name)}
-                  >
-                    {group.name}
-                  </Badge>
-                );
-              })}
-            </div>
-            <DefaultVotingPolicy
-              ratio={dao.policy.defaultVotePolicy.ratio}
-              numberOfGroups={dao.groups.length}
-            />
-          </DaoSetting>
-        )}
-      </div>
-    </div>
+    <NestedDaoPageWrapper
+      daoContext={daoContext}
+      breadcrumbs={breadcrumbs}
+      defaultProposalType={ProposalVariant.ProposeCreateBounty}
+    >
+      <SettingsPageContent daoContext={daoContext} />
+    </NestedDaoPageWrapper>
   );
 };
 
--- pages/dao/[dao]/tasks/polls/PollsPage.tsx
+++ pages/dao/[dao]/tasks/polls/PollsPage.tsx
@@ -1,7 +1,4 @@
-import React, { FC } from 'react';
-
-// Constants
-import { ALL_DAOS_URL } from 'constants/routing';
+import React, { FC, useMemo } from 'react';
 
 // Types
 import { PaginationResponse } from 'types/api';
@@ -14,17 +11,12 @@ import {
 
 // Components
 import { Feed } from 'astro_2.0/features/Feed';
-import { NavLink } from 'astro_2.0/components/NavLink';
-import { BreadCrumbs } from 'astro_2.0/components/BreadCrumbs';
-import { PolicyAffectedWarning } from 'astro_2.0/components/PolicyAffectedWarning';
-import { DaoDetailsMinimized } from 'astro_2.0/components/DaoDetails';
-import { useCreateProposal } from 'astro_2.0/features/CreateProposal/hooks';
+import { NestedDaoPageWrapper } from 'astro_2.0/features/pages/nestedDaoPagesContent/NestedDaoPageWrapper';
 
 // Hooks
-import { useDaoCustomTokens } from 'hooks/useCustomTokens';
+import { useGetBreadcrumbsConfig } from 'hooks/useGetBreadcrumbsConfig';
 
 import { DaoContext } from 'types/context';
-import { NoResultsView } from 'astro_2.0/components/NoResultsView';
 import styles from './Polls.module.scss';
 
 export interface PollsPageProps {
@@ -34,61 +26,40 @@ export interface PollsPageProps {
 }
 
 const PollsPage: FC<PollsPageProps> = ({
-  daoContext: { dao, policyAffectsProposals, userPermissions },
+  daoContext,
   initialPollsData,
   initialProposalsStatusFilterValue,
 }) => {
-  const { tokens } = useDaoCustomTokens();
+  const breadcrumbsConfig = useGetBreadcrumbsConfig(
+    daoContext.dao.id,
+    daoContext.dao.displayName
+  );
 
-  const [CreateProposal, toggleCreateProposal] = useCreateProposal();
+  const breadcrumbs = useMemo(() => {
+    return [
+      breadcrumbsConfig.ALL_DAOS_URL,
+      breadcrumbsConfig.SINGLE_DAO_PAGE,
+      breadcrumbsConfig.POLLS,
+    ];
+  }, [breadcrumbsConfig]);
 
   return (
-    <div className={styles.root}>
-      <BreadCrumbs className={styles.breadcrumbs}>
-        <NavLink href={ALL_DAOS_URL}>All DAOs</NavLink>
-        <NavLink href={`/dao/${dao.id}`}>{dao?.displayName || dao?.id}</NavLink>
-        <span>Polls</span>
-      </BreadCrumbs>
-
-      <div className={styles.dao}>
-        <DaoDetailsMinimized
-          dao={dao}
-          userPermissions={userPermissions}
-          onCreateProposalClick={toggleCreateProposal}
-        />
-
-        <CreateProposal
-          className={styles.createProposal}
-          dao={dao}
-          userPermissions={userPermissions}
-          key={Object.keys(tokens).length}
-          daoTokens={tokens}
-          proposalVariant={ProposalVariant.ProposePoll}
-          showFlag={false}
-          onClose={toggleCreateProposal}
-        />
-
-        <PolicyAffectedWarning
-          data={policyAffectsProposals}
-          className={styles.warningWrapper}
-        />
-      </div>
-
-      {initialPollsData.count === 0 ? (
-        <NoResultsView title="No polls available" />
-      ) : (
-        <Feed
-          title={<h1 className={styles.header}>Polls</h1>}
-          dao={dao}
-          showFlag={false}
-          className={styles.feed}
-          category={ProposalCategories.Polls}
-          initialProposals={initialPollsData}
-          headerClassName={styles.feedHeader}
-          initialProposalsStatusFilterValue={initialProposalsStatusFilterValue}
-        />
-      )}
-    </div>
+    <NestedDaoPageWrapper
+      daoContext={daoContext}
+      breadcrumbs={breadcrumbs}
+      defaultProposalType={ProposalVariant.ProposeCreateBounty}
+    >
+      <Feed
+        title={<h1 className={styles.header}>Polls</h1>}
+        dao={daoContext.dao}
+        showFlag={false}
+        className={styles.feed}
+        category={ProposalCategories.Polls}
+        initialProposals={initialPollsData}
+        headerClassName={styles.feedHeader}
+        initialProposalsStatusFilterValue={initialProposalsStatusFilterValue}
+      />
+    </NestedDaoPageWrapper>
   );
 };
 
