bb779381b416c44a8c54a83126565f24dec21696
Overzunov <overzunov@lohika.com>
2022-02-10 10:11:45 +0200

    feat(dao warning): show low balance warning if dao has less than 5 near tokens

--- astro_2.0/features/pages/nestedDaoPagesContent/NestedDaoPageWrapper/NestedDaoPageWrapper.tsx
+++ astro_2.0/features/pages/nestedDaoPagesContent/NestedDaoPageWrapper/NestedDaoPageWrapper.tsx
@@ -15,6 +15,7 @@ import { NavLink } from 'astro_2.0/components/NavLink';
 import { BreadCrumbs } from 'astro_2.0/components/BreadCrumbs';
 import { DaoDetailsMinimized } from 'astro_2.0/components/DaoDetails';
 import { PolicyAffectedWarning } from 'astro_2.0/components/PolicyAffectedWarning';
+import { DaoWarning } from 'astro_2.0/components/DaoWarning';
 
 import { useDaoCustomTokens } from 'hooks/useCustomTokens';
 import { useCreateProposal } from 'astro_2.0/features/CreateProposal/hooks';
@@ -42,6 +43,8 @@ export const NestedDaoPageWrapper: FC<NestedDaoPageWrapperProps> = props => {
 
   const { tokens } = useDaoCustomTokens();
   const [CreateProposal, toggleCreateProposal] = useCreateProposal();
+  const showLowBalanceWarning =
+    tokens?.NEAR?.balance && Number(tokens?.NEAR?.balance) < 5;
 
   function renderBreadcrumbs() {
     const navItems = breadcrumbs.map(breadcrumb => {
@@ -90,6 +93,20 @@ export const NestedDaoPageWrapper: FC<NestedDaoPageWrapperProps> = props => {
         showFlag={false}
         onClose={toggleCreateProposal}
       />
+      {showLowBalanceWarning && (
+        <DaoWarning
+          content={
+            <>
+              <div className={styles.title}>Error</div>
+              <div className={styles.text}>
+                Your available balance is too low to perform any actions on your
+                account. Please send Near to your account and then try again.
+              </div>
+            </>
+          }
+          className={styles.warningWrapper}
+        />
+      )}
       <PolicyAffectedWarning
         data={policyAffectsProposals}
         className={styles.warningWrapper}
