87213bfb00cffa2c679e84da932ecd5e105c7c35
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2022-02-28 10:28:09 +0200

    fix(create proposal): update fc validation - remove 5 chars limit (#730)
    Co-authored-by: Overzunov <overzunov@lohika.com>

---
+++ hooks/useIntercomAdjust.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+export function useIntercomAdjust(): void {
+  const router = useRouter();
+  const [adjusted, setAdjusted] = useState(false);
+
+  useEffect(() => {
+    // workaround to align intercom button
+    if (!adjusted) {
+      const intercom: HTMLElement | null = document.querySelector(
+        '.intercom-launcher'
+      );
+
+      if (intercom) {
+        intercom.style.bottom = '75px';
+
+        setAdjusted(true);
+      }
+    }
+  }, [adjusted, router.pathname]);
+}
