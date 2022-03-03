59a5c9d1c526983e90cb937f66133cfb83181c01
Overzunov <overzunov@lohika.com>
2022-03-03 12:02:37 +0200

    feat(tgas input): implement shared tgas input component

--- astro_2.0/components/ConfirmActionModal/ConfirmActionModal.tsx
+++ astro_2.0/components/ConfirmActionModal/ConfirmActionModal.tsx
@@ -1,23 +1,15 @@
-import React, { FC, useState } from 'react';
-import cn from 'classnames';
-import { useForm } from 'react-hook-form';
+import React, { FC } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 import { Modal } from 'components/modal';
 import { Button } from 'components/button/Button';
-import { Input } from 'components/inputs/Input';
-import { Icon } from 'components/Icon';
-import { Popup } from 'components/Popup';
+import { TgasInput } from 'astro_2.0/components/TgasInput';
 
 import { gasValidation } from 'astro_2.0/features/CreateProposal/helpers';
 
-import {
-  DEFAULT_PROPOSAL_GAS,
-  DEFAULT_VOTE_GAS,
-  MAX_GAS,
-  MIN_GAS,
-} from 'services/sputnik/constants';
+import { DEFAULT_VOTE_GAS } from 'services/sputnik/constants';
 
 import styles from './ConfirmActionModal.module.scss';
 
@@ -38,7 +30,6 @@ export const ConfirmActionModal: FC<ConfirmActionModalProps> = ({
   title,
   message,
 }) => {
-  const [ref, setRef] = useState<HTMLElement | null>(null);
   const methods = useForm<{ gas: number }>({
     mode: 'all',
     reValidateMode: 'onChange',
@@ -48,93 +39,36 @@ export const ConfirmActionModal: FC<ConfirmActionModalProps> = ({
     resolver: yupResolver(schema),
   });
 
-  const {
-    watch,
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = methods;
+  const { handleSubmit } = methods;
 
   const submitHandler = (data: { gas: number }) => {
-    // console.log(onClose());
     return onClose(data.gas.toString());
   };
 
-  const gas = watch('gas').toString();
-  const error = errors.gas;
-
-  function getInputWidth() {
-    if (gas?.length > 6 && gas?.length <= 10) {
-      return `${gas?.length}ch`;
-    }
-
-    if (gas?.length > 10) {
-      return '10ch';
-    }
-
-    return '6ch';
-  }
-
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="sm">
-      <div className={styles.root}>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.message}>{message}</div>
+      <FormProvider {...methods}>
+        <div className={styles.root}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.message}>{message}</div>
 
-        <form
-          noValidate
-          className={styles.content}
-          onSubmit={handleSubmit(submitHandler)}
-        >
-          <div>
-            <div
-              className={cn(styles.inputLabel, {
-                [styles.labelError]: !!error,
-              })}
-            >
-              Tgas
-              {!!error && (
-                <>
-                  <div ref={setRef} className={styles.errorIcon}>
-                    <Icon name="info" width={12} className={styles.icon} />
-                  </div>
-                  <Popup anchor={ref} offset={[0, 10]} placement="right">
-                    <span className={styles.errorMessage}>{error.message}</span>
-                  </Popup>
-                </>
-              )}
-            </div>
-            <div className={styles.row}>
-              <Input
-                className={cn(styles.inputWrapper, styles.detailsInput, {
-                  [styles.error]: !!error,
-                })}
-                inputStyles={{
-                  width: getInputWidth(),
-                }}
-                onClick={e => e.stopPropagation()}
-                type="number"
-                min={MIN_GAS}
-                step={1}
-                max={MAX_GAS}
-                isBorderless
-                size="block"
-                placeholder={`${DEFAULT_PROPOSAL_GAS}`}
-                data-testid="gas-input"
-                {...register('gas')}
-              />
-            </div>
-          </div>
-          <Button
-            capitalize
-            type="submit"
-            data-testid="close-button"
-            className={styles.confirmButton}
+          <form
+            noValidate
+            className={styles.content}
+            onSubmit={handleSubmit(submitHandler)}
           >
-            Confirm
-          </Button>
-        </form>
-      </div>
+            <TgasInput />
+            <Button
+              capitalize
+              type="submit"
+              data-testid="close-button"
+              className={styles.confirmButton}
+            >
+              Confirm
+            </Button>
+          </form>
+        </div>
+      </FormProvider>
     </Modal>
   );
 };
