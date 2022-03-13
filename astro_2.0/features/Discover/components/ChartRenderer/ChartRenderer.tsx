b1b69f4312ca6dfa2fa8238e5cd93d94c7d66a39
Overzunov <overzunov@lohika.com>
2022-03-13 16:17:28 +0200

    feat(discover page): add scroll behavior to topics cards

--- astro_2.0/features/Discover/components/ChartRenderer/ChartRenderer.tsx
+++ astro_2.0/features/Discover/components/ChartRenderer/ChartRenderer.tsx
@@ -3,6 +3,8 @@ import { DashboardChart } from 'astro_2.0/features/DaoDashboard/components/Dashb
 import { Loader } from 'components/loader';
 import { NoResultsView } from 'astro_2.0/components/NoResultsView';
 import { ChartDataElement } from 'components/AreaChartRenderer/types';
+import { useDiscoverPageRange } from 'astro_2.0/features/Discover/components/DiscoverPageContext/DiscoverPageContext';
+import { DOMAIN_RANGES } from 'components/AreaChartRenderer/helpers';
 
 import styles from './ChartRenderer.module.scss';
 
@@ -12,18 +14,42 @@ interface ChartRendererProps {
   activeView: string;
 }
 
+const RANGE_SET = [
+  {
+    label: '1M',
+    type: DOMAIN_RANGES.MONTH,
+  },
+  {
+    label: '1Y',
+    type: DOMAIN_RANGES.YEAR,
+  },
+  {
+    label: 'All',
+    type: DOMAIN_RANGES.ALL,
+  },
+];
+
 export const ChartRenderer: FC<ChartRendererProps> = ({
   data,
   loading,
   activeView,
 }) => {
+  const { range, setRange } = useDiscoverPageRange();
+
   if (loading) {
     return <Loader className={styles.loader} />;
   }
 
   if (data) {
     return (
-      <DashboardChart key={activeView} activeView={activeView} data={data} />
+      <DashboardChart
+        key={activeView}
+        activeView={activeView}
+        data={data}
+        timeRanges={RANGE_SET}
+        initialRange={range}
+        onRangeChange={setRange}
+      />
     );
   }
 
--- astro_2.0/features/Discover/components/ControlTabs/ControlTabs.tsx
+++ astro_2.0/features/Discover/components/ControlTabs/ControlTabs.tsx
@@ -1,10 +1,16 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useCallback, useRef, useState } from 'react';
 import cn from 'classnames';
+import { useMeasure } from 'react-use';
+import { useSwipeable } from 'react-swipeable';
 
+import { IconButton } from 'components/button/IconButton';
 import { StatCard } from 'astro_2.0/features/DaoDashboard/components/StatCard';
 import { StatPanel } from 'astro_2.0/features/DaoDashboard/components/StatPanel';
 
 import { TControlTab } from 'astro_2.0/features/Discover/types';
+
+import { useWindowResize } from 'hooks/useWindowResize';
+import { useSyncRefs } from 'hooks/useSyncRefs';
 
 import styles from './ControlTabs.module.scss';
 
@@ -16,51 +22,137 @@ interface ControlTabsProps {
   loading: boolean;
 }
 
+function getContainerProps(width: number): [number, number] {
+  if (width < 400) {
+    return [1, 0];
+  }
+
+  if (width < 500) {
+    return [2, 1];
+  }
+
+  return [3, 2];
+}
+
 export const ControlTabs: FC<ControlTabsProps> = ({
   items,
   onSelect,
   activeView,
   className,
-  loading,
 }) => {
-  useEffect(() => {
+  const rootRef = useRef<HTMLDivElement>(null);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const [ref, { width }] = useMeasure();
+  const CARD_GAP = 12;
+  const [NUM_VISIBLE_CARDS, GAPS_COUNT] = getContainerProps(width);
+  const cardWidth = Math.floor(
+    (width - CARD_GAP * GAPS_COUNT) / NUM_VISIBLE_CARDS
+  );
+
+  const handleNavigate = useCallback(
+    (isForward: boolean) => {
+      if (rootRef.current) {
+        const currentScrollLeft = rootRef.current.scrollLeft;
+
+        const scroll = isForward
+          ? currentScrollLeft + cardWidth + 12
+          : currentScrollLeft - cardWidth - 12;
+
+        const newScroll = scroll < 0 ? 0 : scroll;
+
+        rootRef.current.scroll({
+          left: newScroll,
+          top: 0,
+          behavior: 'smooth',
+        });
+
+        setScrollLeft(newScroll);
+      }
+    },
+    [cardWidth]
+  );
+
+  const onWindowResize = useCallback(() => {
     const selectedItem = document.querySelector(`.${styles.active}`);
 
     if (selectedItem) {
       setTimeout(() => {
         selectedItem.scrollIntoView({
-          behavior: 'smooth',
           block: 'nearest',
           inline: 'center',
         });
+
+        if (rootRef.current) {
+          const currentScrollLeft = rootRef?.current?.scrollLeft;
+
+          setScrollLeft(currentScrollLeft);
+        }
       }, 500);
     }
-  }, [activeView]);
+  }, []);
+
+  const mergeRefs = useSyncRefs(rootRef, ref);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      handleNavigate(true);
+    },
+    onSwipedRight: () => {
+      handleNavigate(false);
+    },
+  });
+
+  useWindowResize(onWindowResize);
 
   return (
-    <div className={cn(styles.root, className)}>
-      {items.map(({ id, label, value, trend }) => (
-        <StatCard
-          key={id}
-          selected={activeView === id}
-          className={cn(styles.card, {
-            [styles.active]: activeView === id,
-            [styles.disabled]: loading,
-          })}
-          disabled={loading}
-          onClick={() => onSelect(id)}
-        >
-          <StatPanel
-            title={label}
-            value={value}
-            trend={trend}
-            className={styles.cardPanel}
-            titleClassName={styles.cardTitle}
-            trendClassName={styles.trend}
-            valueClassName={styles.value}
+    <div {...handlers}>
+      {items.length > NUM_VISIBLE_CARDS && (
+        <div className={styles.scrollControls}>
+          <IconButton
+            icon="buttonArrowLeft"
+            size="medium"
+            disabled={scrollLeft <= 0}
+            className={cn(styles.scrollControlButton, {
+              [styles.disabled]: scrollLeft <= 0,
+            })}
+            onClick={() => handleNavigate(false)}
           />
-        </StatCard>
-      ))}
+          <IconButton
+            icon="buttonArrowRight"
+            size="medium"
+            disabled={scrollLeft + width >= cardWidth * items.length}
+            className={cn(styles.scrollControlButton, {
+              [styles.disabled]: scrollLeft + width >= cardWidth * items.length,
+            })}
+            onClick={() => handleNavigate(true)}
+          />
+        </div>
+      )}
+      <div className={cn(styles.root, className)} ref={mergeRefs}>
+        {items.map(({ id, label, value, trend }) => (
+          <StatCard
+            key={id}
+            selected={activeView === id}
+            className={cn(styles.card, {
+              [styles.active]: activeView === id,
+            })}
+            style={{
+              maxWidth: cardWidth,
+            }}
+            onClick={() => onSelect(id)}
+          >
+            <StatPanel
+              title={label}
+              value={value}
+              trend={trend}
+              className={styles.cardPanel}
+              titleClassName={styles.cardTitle}
+              trendClassName={styles.trend}
+              valueClassName={styles.value}
+            />
+          </StatCard>
+        ))}
+      </div>
     </div>
   );
 };
