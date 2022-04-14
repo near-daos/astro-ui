bbf116b907ee24b01c832f94dd1e74cfe30dd56f
Overzunov <overzunov@lohika.com>
2022-04-14 19:15:09 +0300

    fix(feed): exclude proposals related to dao upgrade process

--- services/HttpService/HttpService.ts
+++ services/HttpService/HttpService.ts
@@ -35,6 +35,7 @@ import {
   ProposalCategories,
   ProposalsFeedStatuses,
   ProposalType,
+  ProposalVariant,
 } from 'types/proposal';
 import { DaoFeedItem } from 'types/dao';
 
@@ -791,6 +792,27 @@ export class HttpService {
                 ],
               });
             }
+
+            // // Exclude proposals related to DAO upgrade as they are available only in settings upgrade wizard
+            search.$and?.push({
+              $and: [
+                {
+                  description: {
+                    $exclL: ProposalVariant.ProposeUpgradeSelf,
+                  },
+                },
+                {
+                  description: {
+                    $exclL: ProposalVariant.ProposeGetUpgradeCode,
+                  },
+                },
+                {
+                  description: {
+                    $exclL: ProposalVariant.ProposeRemoveUpgradeCode,
+                  },
+                },
+              ],
+            });
 
             if (search.$and?.length) {
               queryString.search(search);
