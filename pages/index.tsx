453b66b81c41a2ebbe8527b8af36118547db9281
maxmagic-dev <Maksym.Vasylenko@magicpowered.io>
2022-04-21 19:27:45 +0300

    feat(dev): build fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,47 @@
-import 'assets/icons';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { GetServerSideProps } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export default function RootPage(): JSX.Element {
+import nextI18NextConfig from 'next-i18next.config';
+
+import 'assets/icons';
+
+import { CookieService } from 'services/CookieService';
+
+import { ALL_FEED_URL, MY_FEED_URL } from 'constants/routing';
+import { ACCOUNT_COOKIE } from 'constants/cookies';
+import { useRouter } from 'next/router';
+
+type Props = {
+  account: string | undefined;
+};
+
+export default function RootPage({ account }: Props): JSX.Element {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(account ? MY_FEED_URL : ALL_FEED_URL);
+  }, [account, router]);
+
   return <div />;
 }
 
+export const getServerSideProps: GetServerSideProps = async ({
+  locale = 'en',
+}) => {
+  const account = CookieService.get<string | undefined>(ACCOUNT_COOKIE);
+
+  return {
+    props: {
+      ...(await serverSideTranslations(
+        locale,
+        ['common', 'notificationsPage'],
+        nextI18NextConfig
+      )),
+      account,
+    },
+  };
+};
 /*
 export const getServerSideProps: GetServerSideProps = async ctx => {
   return {
