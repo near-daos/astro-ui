8ef840cf94549cb577d73373974b998d1a04b713
Overzunov <overzunov@lohika.com>
2022-04-29 21:32:29 +0300

    fix(storybook): fix sb build

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,6 @@
 import '!style-loader!css-loader!sass-loader!../styles/globals.scss';
 import * as nextImage from 'next/image';
-import { RouterContext } from 'next/dist/next-server/lib/router-context';
-import { withNextRouter } from 'storybook-addon-next-router';
-import { addDecorator } from '@storybook/react';
-
-addDecorator(
-  withNextRouter({
-    path: '/', // defaults to `/`
-    asPath: '/', // defaults to `/`
-    query: {}, // defaults to `{}`
-    push() {}, // defaults to using addon actions integration, can override any method in the router
-  })
-);
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 
 // To allow next image component to be displayed inside of storybook
 Object.defineProperty(nextImage, 'default', {
