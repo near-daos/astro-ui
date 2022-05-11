2858394fbd3a9ddea3225181e047d659648c68d4
AlexeyPonomarenko <alexey.a.ponomarenko@gmail.com>
2022-05-11 20:45:01 +0300

    test(notifications): updated ui of notifications bell

--- astro_2.0/components/AppHeader/components/NotificationsBell/NotificationsBell.tsx
+++ astro_2.0/components/AppHeader/components/NotificationsBell/NotificationsBell.tsx
@@ -1,6 +1,5 @@
 import cn from 'classnames';
-import { useHoverDirty } from 'react-use';
-import { useRef, VFC } from 'react';
+import { VFC } from 'react';
 import Link from 'next/link';
 
 import { FEATURE_FLAGS } from 'constants/featureFlags';
@@ -19,41 +18,24 @@ interface NotificationsBellProps {
 export const NotificationsBell: VFC<NotificationsBellProps> = ({
   className,
 }) => {
-  const rootRef = useRef(null);
-  const isHovered = useHoverDirty(rootRef);
-
-  const counter = useNotificationsCount();
-
-  function renderBellIcon() {
-    if (!counter) {
-      return (
-        <Icon
-          name="noteBell"
-          className={styles.bell}
-          data-testid="no-notifications-icon"
-        />
-      );
-    }
-
-    return (
-      <>
-        <Icon
-          className={styles.bell}
-          data-testid="notifications-icon"
-          name={isHovered ? 'noteBellActiveHover' : 'noteBellActive'}
-        />
-        <div className={styles.notificationsCount}>
-          {counter > 99 ? '99+' : counter}
-        </div>
-      </>
-    );
-  }
+  const counter = useNotificationsCount() || 0;
 
   return FEATURE_FLAGS.NOTIFICATIONS ? (
     <>
       <Link href="/notifications" passHref>
-        <div className={cn(styles.root, className)} ref={rootRef}>
-          {renderBellIcon()}
+        <div className={cn(styles.root, className)}>
+          <Icon
+            name="noteBell"
+            className={styles.bell}
+            data-testid="no-notifications-icon"
+          />
+          <div
+            className={cn(styles.notificationsCount, {
+              [styles.visible]: counter > 0,
+            })}
+          >
+            {counter}
+          </div>
         </div>
       </Link>
       <NotificationsToastsContainer />
