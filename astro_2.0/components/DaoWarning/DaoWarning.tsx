f5987238fc68dc4d9fbaf1fb3244fec754270932
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2022-05-16 19:12:26 +0300

    fix(upgrade): fix feed (#1084)
    Co-authored-by: Overzunov <overzunov@lohika.com>

--- astro_2.0/components/DaoWarning/DaoWarning.tsx
+++ astro_2.0/components/DaoWarning/DaoWarning.tsx
@@ -1,4 +1,5 @@
 import React, { FC, ReactNode } from 'react';
+import cn from 'classnames';
 
 import { Icon } from 'components/Icon';
 
@@ -8,18 +9,24 @@ interface DaoWarningProps {
   content: ReactNode;
   control?: ReactNode;
   className?: string;
+  rootClassName?: string;
+  statusClassName?: string;
+  iconClassName?: string;
 }
 
 export const DaoWarning: FC<DaoWarningProps> = ({
   content,
   control,
   className,
+  rootClassName,
+  statusClassName,
+  iconClassName,
 }) => {
   return (
     <div className={className}>
-      <div className={styles.root}>
-        <div className={styles.status}>
-          <Icon name="info" className={styles.icon} />
+      <div className={cn(styles.root, rootClassName)}>
+        <div className={cn(styles.status, statusClassName)}>
+          <Icon name="info" className={cn(styles.icon, iconClassName)} />
         </div>
         <div className={styles.content}>{content}</div>
         {control && <div className={styles.control}>{control}</div>}
