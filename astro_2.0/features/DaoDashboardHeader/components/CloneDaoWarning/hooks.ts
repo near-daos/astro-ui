d933050bb0d8065f2d98b09c7b4723fc68b95b11
Overzunov <overzunov@lohika.com>
2022-06-01 17:25:48 +0300

    fix(migrate dao): update warnings, add dismiss action and modal

--- astro_2.0/features/DaoDashboardHeader/components/CloneDaoWarning/hooks.ts
+++ astro_2.0/features/DaoDashboardHeader/components/CloneDaoWarning/hooks.ts
@@ -1,9 +1,10 @@
 import { useWalletContext } from 'context/WalletContext';
 import { useEffect, useState } from 'react';
 import { Settings } from 'types/settings';
-import { useAsyncFn } from 'react-use';
+import { useAsyncFn, useMount } from 'react-use';
 import { SputnikHttpService } from 'services/sputnik';
 import { NOTIFICATION_TYPES, showNotification } from 'features/notifications';
+import { YOKTO_NEAR } from 'services/sputnik/constants';
 
 export function useDaoSettings(
   daoId: string
@@ -73,4 +74,28 @@ export function useDaoSettings(
     update,
     settings,
   };
+}
+
+const STORAGE_PRICE_PER_BYTE = 10_000_000_000_000_000_000;
+
+export function useAccountState(): number | null {
+  const { nearService } = useWalletContext();
+  const [storagePrice, setStoragePrice] = useState<number | null>(null);
+
+  const [, getState] = useAsyncFn(async () => {
+    const res = nearService?.getAccount();
+
+    if (res) {
+      const state = await res.state();
+      const { storage_usage: storageUsage } = state;
+
+      const nearPrice = (STORAGE_PRICE_PER_BYTE * storageUsage) / YOKTO_NEAR;
+
+      setStoragePrice(nearPrice);
+    }
+  }, []);
+
+  useMount(() => getState());
+
+  return storagePrice;
 }
