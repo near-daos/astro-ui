246047a47aa86ddb3f087835f85ee2c0d17584fc
AlexeyPonomarenko <alexey.a.ponomarenko@gmail.com>
2022-06-12 19:33:54 +0300

    test(tests): another portion of tests

---
+++ astro_2.0/features/CreateProposal/components/CustomFunctionCallContent/components/VoteInOtherDao/tests/VoteInOtherDao.spec.tsx
@@ -0,0 +1,28 @@
+import { render } from 'jest/testUtils';
+
+import { DAO } from 'types/dao';
+
+import { VoteInOtherDao } from 'astro_2.0/features/CreateProposal/components/CustomFunctionCallContent/components/VoteInOtherDao';
+
+jest.mock('react-hook-form', () => {
+  return {
+    useFormContext: jest.fn(() => ({
+      watch: () => 1,
+      register: () => ({}),
+      setValue: () => ({}),
+      formState: {
+        errors: {},
+      },
+    })),
+  };
+});
+
+describe('VoteInOtherDao', () => {
+  it('Should render component', () => {
+    const dao = ({} as unknown) as DAO;
+
+    const { getByText } = render(<VoteInOtherDao dao={dao} />);
+
+    expect(getByText('proposalCard.voteInDao.targetDao.label')).toBeTruthy();
+  });
+});
