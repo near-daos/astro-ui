ec9482853812f8dfc14efa1209b34dad79911036
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2022-06-15 18:11:05 +0300

    fix(fix wizard): fix roles permissions (#1191)
    Co-authored-by: Overzunov <overzunov@lohika.com>

--- astro_2.0/features/CreateDao/components/DaoSubmitForm/helpers.ts
+++ astro_2.0/features/CreateDao/components/DaoSubmitForm/helpers.ts
@@ -98,7 +98,7 @@ export function getDetailedRolesVotingPolicy(
   // todo - use groups here
   // const oldGroups = ((proposals.data ?? []) as unknown) as { label: string }[];
 
-  const roles: DaoRole[] = [{ name: 'all', slug: 'all' }, ...groups]
+  const roles: DaoRole[] = [...groups]
     .map(item => {
       if (item.slug === 'all') {
         return {
