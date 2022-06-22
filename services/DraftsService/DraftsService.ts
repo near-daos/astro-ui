c11d3b177092a213db29e4b5fa67eaf4af048324
Viacheslav <steelhouse.www.123@gmail.com>
2022-06-22 20:04:29 +0300

    fix(draft page): change data from mock to real data in the view page (#1214)

--- services/DraftsService/DraftsService.ts
+++ services/DraftsService/DraftsService.ts
@@ -22,8 +22,10 @@ export class DraftsService {
     }/api/v1/`,
   });
 
-  constructor(httpService: HttpService) {
-    this.httpService = httpService;
+  constructor(httpService?: HttpService) {
+    if (httpService) {
+      this.httpService = httpService;
+    }
   }
 
   // Draft
@@ -51,11 +53,13 @@ export class DraftsService {
     id: string,
     accountId?: string
   ): Promise<AxiosResponse<DraftProposal>> {
-    return this.httpService.get(`/draft-proposals/${id}`, {
+    const { data } = await this.httpService.get(`/draft-proposals/${id}`, {
       params: {
         accountId,
       },
     });
+
+    return data;
   }
 
   public async patchDraft(data: DraftProposal): Promise<AxiosResponse<string>> {
