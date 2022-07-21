cd79dffc0936a73c3bca8e20687b397e36eeab9c
Overzunov <overzunov@lohika.com>
2022-07-21 17:01:02 +0300

    fix(proposal page): add sorting, add explorer links

--- features/proposal/components/VoteTimeline/VoteAction/VoteAction.tsx
+++ features/proposal/components/VoteTimeline/VoteAction/VoteAction.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
+import Link from 'next/link';
+
+import { configService } from 'services/ConfigService';
 
 import { Tooltip } from 'astro_2.0/components/Tooltip';
 import { Icon } from 'components/Icon';
@@ -13,6 +16,7 @@ export type VoteActionProps = {
   position: string;
   date: string;
   name: string;
+  hash?: string;
 };
 
 export const VoteAction: FC<VoteActionProps> = ({
@@ -21,28 +25,44 @@ export const VoteAction: FC<VoteActionProps> = ({
   position = '0',
   date,
   name,
-}) => (
-  <div
-    style={{ left: position }}
-    className={cn(
-      styles.voteAction,
-      {
-        [styles.failure]: type === 'VoteReject',
-        [styles.approve]: type === 'VoteApprove',
-      },
-      className
-    )}
-  >
-    <Tooltip
-      popupClassName={styles.tooltip}
-      overlay={
-        <>
-          {name}
-          <br /> {date}
-        </>
-      }
+  hash,
+}) => {
+  const { nearConfig } = configService.get();
+
+  const explorerLink = hash
+    ? `${nearConfig?.explorerUrl}/transactions/${hash}`
+    : '';
+
+  return (
+    <div
+      style={{ left: position }}
+      className={cn(
+        styles.voteAction,
+        {
+          [styles.failure]: type === 'VoteReject',
+          [styles.approve]: type === 'VoteApprove',
+        },
+        className
+      )}
     >
-      <Icon name={type === 'VoteApprove' ? 'votingYes' : 'votingNo'} />
-    </Tooltip>
-  </div>
-);
+      <Tooltip
+        popupClassName={styles.tooltip}
+        overlay={
+          <span className={styles.tooltipOverlay}>
+            {name}
+            <br />
+            {date}
+            <br />
+            <span className={styles.sub}>Click to open in Explorer</span>
+          </span>
+        }
+      >
+        <Link href={explorerLink}>
+          <a rel="noreferrer" target="_blank">
+            <Icon name={type === 'VoteApprove' ? 'votingYes' : 'votingNo'} />
+          </a>
+        </Link>
+      </Tooltip>
+    </div>
+  );
+};
