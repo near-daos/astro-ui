f62feef1fe36412b3a209bf0c99bc980ff79d113
Max <maksym.vasylenko@magicpowered.io>
2022-08-03 18:59:34 +0300

    feat(dev): add alternative wrapper for Action button

--- astro_2.0/components/ActionButton/ActionButton.tsx
+++ astro_2.0/components/ActionButton/ActionButton.tsx
@@ -19,7 +19,37 @@ interface ActionButtonProps {
   tooltip?: string;
   tooltipPlacement?: 'right' | 'top' | 'bottom' | 'left' | 'auto';
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  href?: string;
 }
+
+const Wrapper = ({
+  className,
+  addRef,
+  link,
+  children,
+  ...rest
+}: {
+  className?: string;
+  link?: string;
+  addRef: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
+  children: React.ReactElement;
+}) =>
+  link ? (
+    // eslint-disable-next-line jsx-a11y/control-has-associated-label
+    <a
+      href={link}
+      ref={addRef}
+      className={className}
+      {...rest}
+      style={{ display: 'inline-block' }}
+    >
+      {children}
+    </a>
+  ) : (
+    <div ref={addRef} className={className} {...rest}>
+      {children}
+    </div>
+  );
 
 export const ActionButton: FC<ActionButtonProps> = ({
   testId,
@@ -33,12 +63,13 @@ export const ActionButton: FC<ActionButtonProps> = ({
   tooltip,
   tooltipPlacement = 'auto',
   children,
+  href,
 }) => {
   const [ref, setRef] = useState<HTMLElement | null>(null);
 
   return (
     <>
-      <div ref={setRef} className={className}>
+      <Wrapper addRef={setRef} className={className} link={href}>
         {children ? (
           <IconTextButton
             testId={testId}
@@ -60,7 +91,7 @@ export const ActionButton: FC<ActionButtonProps> = ({
             }}
           />
         )}
-      </div>
+      </Wrapper>
       {tooltip && !disabled && (
         <Popup
           anchor={ref}
