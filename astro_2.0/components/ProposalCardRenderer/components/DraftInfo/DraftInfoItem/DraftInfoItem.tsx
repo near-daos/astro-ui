1010cfa6d5cd20abc32363077c00103ae497aaf1
Viacheslav Serhiienko <steelhouse.www.123@gmail.com>
2022-08-06 18:08:24 +0300

    feat(drafts): add tooltip to the save draft button

--- astro_2.0/components/ProposalCardRenderer/components/DraftInfo/DraftInfoItem/DraftInfoItem.tsx
+++ astro_2.0/components/ProposalCardRenderer/components/DraftInfo/DraftInfoItem/DraftInfoItem.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import cn from 'classnames';
 
 import { Icon, IconName } from 'components/Icon';
+import { Tooltip } from 'astro_2.0/components/Tooltip';
 
 import styles from './DraftInfoItem.module.scss';
 
@@ -11,6 +12,7 @@ interface DraftInfoItemProps {
   iconName: IconName;
   onClick?: () => void;
   disabled?: boolean;
+  tooltipText?: string;
 }
 
 export const DraftInfoItem: FC<DraftInfoItemProps> = ({
@@ -19,11 +21,18 @@ export const DraftInfoItem: FC<DraftInfoItemProps> = ({
   iconName,
   onClick,
   disabled,
+  tooltipText,
 }) => {
   const infoItem = (
     <>
       <div className={styles.count}>{count}</div>
-      <Icon name={iconName} className={styles.icon} />
+      {tooltipText ? (
+        <Tooltip placement="top" overlay={<span>Save Draft</span>}>
+          <Icon name={iconName} className={styles.icon} />
+        </Tooltip>
+      ) : (
+        <Icon name={iconName} className={styles.icon} />
+      )}
     </>
   );
 
