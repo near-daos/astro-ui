ac7da91322cbe2a6376e60a988b6d1ac24dc25e9
Overzunov <overzunov@lohika.com>
2022-08-30 18:07:48 +0300

    fix(feed): update icons, update filters logic

--- astro_3.0/features/ProposalsFeed/components/CategoriesFeedFilter/CategoriesFeedFilter.tsx
+++ astro_3.0/features/ProposalsFeed/components/CategoriesFeedFilter/CategoriesFeedFilter.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import cn from 'classnames';
 import { useRouter } from 'next/router';
+import intersection from 'lodash/intersection';
 
 import { Button } from 'components/button/Button';
 import { Icon, IconName } from 'components/Icon';
@@ -34,6 +35,22 @@ export const CategoriesFeedFilter: FC<Props> = ({
 }) => {
   const { query, replace } = useRouter();
 
+  const queriesNames = useMemo<string[]>(() => {
+    if (!list) {
+      return [];
+    }
+
+    const values = list.reduce<Set<string>>((res, item) => {
+      if (item.queryName) {
+        res.add(item.queryName);
+      }
+
+      return res;
+    }, new Set());
+
+    return Array.from(values);
+  }, [list]);
+
   function renderFilterItem(item: ListItem) {
     const {
       value: itemVal,
@@ -57,6 +74,7 @@ export const CategoriesFeedFilter: FC<Props> = ({
       {
         [styles.active]: value === itemVal || (!itemVal && !value),
         [styles.disabled]: disabled || disabledItem,
+        [styles.hidden]: value && value !== itemVal,
       },
       itemClassName
     );
@@ -80,8 +98,51 @@ export const CategoriesFeedFilter: FC<Props> = ({
     );
   }
 
+  function isSelected() {
+    const q = Object.keys(query);
+    const activeQ = q.reduce<string[]>((res, item) => {
+      if (query[item]) {
+        res.push(item);
+      }
+
+      return res;
+    }, []);
+
+    return intersection(activeQ, queriesNames).length > 0;
+  }
+
   return (
-    <div className={cn(styles.root, className)}>
+    <div
+      className={cn(styles.root, className, {
+        [styles.selected]: isSelected(),
+      })}
+    >
+      <div className={styles.close}>
+        <Button
+          variant="transparent"
+          size="block"
+          className={styles.closeBtn}
+          onClick={async () => {
+            const href = {
+              query: {
+                ...query,
+                ...queriesNames.reduce<Record<string, string>>((res, item) => {
+                  res[item] = '';
+
+                  return res;
+                }, {}),
+              },
+            };
+
+            await replace(href, undefined, {
+              shallow: shallowUpdate,
+              scroll: false,
+            });
+          }}
+        >
+          <Icon name="close" className={styles.closeIcon} />
+        </Button>
+      </div>
       <ul className={styles.items}>{list?.map(renderFilterItem)}</ul>
     </div>
   );
