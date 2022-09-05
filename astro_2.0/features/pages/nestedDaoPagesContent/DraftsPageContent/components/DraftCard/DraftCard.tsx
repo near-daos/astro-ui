63ab736ea58a756dbd4173d6b1f015a58b01e7ea
Overzunov <overzunov@lohika.com>
2022-09-05 19:38:36 +0300

    feat(drafts): add multiselect and removw to drafts list

--- astro_2.0/features/pages/nestedDaoPagesContent/DraftsPageContent/components/DraftCard/DraftCard.tsx
+++ astro_2.0/features/pages/nestedDaoPagesContent/DraftsPageContent/components/DraftCard/DraftCard.tsx
@@ -13,16 +13,35 @@ interface Props {
   data: DraftProposalFeedItem;
   flag: string;
   daoId: string;
+  onSelect: (id: string) => void;
+  selectedList: string[];
+  disableEdit: boolean;
 }
 
-export const DraftCard: FC<Props> = ({ data, flag, daoId }) => {
+export const DraftCard: FC<Props> = ({
+  data,
+  flag,
+  daoId,
+  onSelect,
+  selectedList,
+  disableEdit,
+}) => {
   return (
     <div className={styles.root}>
       <ProposalCardRenderer
+        nonActionable={selectedList && selectedList?.length > 0 && disableEdit}
         className={styles.card}
         showInfo={false}
         daoFlagNode={null}
-        proposalCardNode={<DraftCardContent data={data} daoId={daoId} />}
+        proposalCardNode={
+          <DraftCardContent
+            data={data}
+            daoId={daoId}
+            onSelect={onSelect}
+            selectedList={selectedList}
+            disableEdit={disableEdit}
+          />
+        }
         infoPanelNode={null}
         letterHeadNode={
           <LetterHeadWidget
