b86379aca830fd632941d78f1e2a1770a74efddd
Overzunov <overzunov@lohika.com>
2022-10-17 11:49:31 +0300

    fix(sidebar): fix dao avatars distorsion

--- astro_3.0/features/MobileAppNavigation/MobileAppNavigation.tsx
+++ astro_3.0/features/MobileAppNavigation/MobileAppNavigation.tsx
@@ -32,7 +32,7 @@ export const MobileAppNavigation: FC = () => {
         href: CREATE_DAO_URL,
       },
       {
-        icon: 'sidebarBounties',
+        icon: 'briefcase',
         label: 'Bounties area',
         href: 'none',
       },
