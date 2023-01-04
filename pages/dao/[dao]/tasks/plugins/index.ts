ee162aba9b196842a4e77b105fdb965882ecf2e1
Overzunov <overzunov@lohika.com>
2023-01-04 16:31:44 +0200

    refactor(app setup): cleanup dependencies, fix plugins page crash

--- pages/dao/[dao]/tasks/plugins/index.ts
+++ pages/dao/[dao]/tasks/plugins/index.ts
@@ -1,3 +1,25 @@
-import Plugins from 'pages/dao/[dao]/tasks/plugins/PluginsPage';
+import { GetServerSideProps } from 'next';
 
-export default Plugins;
+import { CookieService } from 'services/CookieService';
+
+import { getTranslations } from 'utils/getTranslations';
+import { getDefaultAppVersion } from 'utils/getDefaultAppVersion';
+
+import { PluginPageProps } from './PluginsPage';
+
+export const getServerSideProps: GetServerSideProps<PluginPageProps> = async ({
+  req,
+  locale = 'en',
+}) => {
+  CookieService.initServerSideCookies(req?.headers.cookie || null);
+
+  return {
+    props: {
+      ...(await getTranslations(locale)),
+      ...(await getDefaultAppVersion()),
+      plugins: [],
+    },
+  };
+};
+
+export { default } from './PluginsPage';
